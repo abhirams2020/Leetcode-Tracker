6fe0e0e68404c291a5e2eb7e4fb5e2b0a0b64a12
Abhirams2020 <58247649+Abhirams2020@users.noreply.github.com>
2022-03-16 16:07:09 +0530

    Time: 100 ms (80.37%), Space: 53.6 MB (16.24%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -3,11 +3,12 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_set<int> s;
         for(auto i:nums){
-            if(s.count(i)!=0){
-                return true;
-            }
+            // if(s.count(i)!=0){
+            //     return true;
+            // }
             s.insert(i);
         }
+        return nums.size()>s.size();
         return false;
     }
 };
