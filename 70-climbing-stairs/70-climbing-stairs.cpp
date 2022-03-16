2e863511531f55edd04149d2817f01f177010a65
Abhirams2020 <58247649+Abhirams2020@users.noreply.github.com>
2022-03-16 17:03:51 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (29.23%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
-        // vector<int> count(n+1,0);
-        // count[0] = 1;
-        // count[1] = 1;
-        // for(int i=2;i<=n;i++){
-        //     count[i] = count[i-1] + count[i-2];
-        // }
-        // return count[n];
-        int dp[46];
-        int solve(int n){
-            if(n<0){
-                return 0;
-            }
-            if(n==0){
-                return 1;
-            }
-            if(dp[n]!=-1){
-                return dp[n];
-            }
-            return dp[n]=solve(n-1) + solve(n-2);        
+    int climbStairs(int n) {
+        vector<int> count(n+1,0);
+        count[0] = 1;
+        count[1] = 1;
+        for(int i=2;i<=n;i++){
+            count[i] = count[i-1] + count[i-2];
         }
-        
-        int climbStairs(int n) {
-            memset(dp,-1,sizeof(dp));
-            return solve(n);
-        }
+        return count[n];
+    }
+//     int dp[46];
+//     int solve(int n){
+//         if(n<0){
+//             return 0;
+//         }
+//         if(n==0){
+//             return 1;
+//         }
+//         if(dp[n]!=-1){
+//             return dp[n];
+//         }
+//         return dp[n]=solve(n-1) + solve(n-2);        
+//     }
+
+//     int climbStairs(int n) {
+//         memset(dp,-1,sizeof(dp));
+//         return solve(n);
+//     }
 };
