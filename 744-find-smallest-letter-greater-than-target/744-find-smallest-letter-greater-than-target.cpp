e610708c55359a10d80944a8f4a88971ce616cb5
Abhirams2020 <58247649+Abhirams2020@users.noreply.github.com>
2022-03-17 15:05:25 +0530

    Time: 20 ms (59.83%), Space: 15.9 MB (26.42%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
@@ -1,19 +1,43 @@
 class Solution {
 public:
+    // // naive solution O(n)
+    // char nextGreatestLetter(vector<char>& letters, char target) {
+    //     char minchar;
+    //     unordered_set<char> s;
+    //     for(auto i:letters){
+    //         s.insert(i);
+    //     }
+    //     int index = target - 'a';
+    //     while(1){
+    //         if(s.find('a'+index)!=s.end() && target!='a'+index){
+    //             minchar = 'a' + index;
+    //             break;
+    //         }
+    //         index = (index+1)%26;
+    //     }
+    //     return minchar;
+    // }
+    
+
     char nextGreatestLetter(vector<char>& letters, char target) {
+        int size = letters.size();
+        if(target<letters[0] || target>=letters[letters.size()-1]){
+            return letters[0];
+        }
         char minchar;
-        unordered_set<char> s;
-        for(auto i:letters){
-            s.insert(i);
-        }
-        int index = target - 'a';
-        while(1){
-            if(s.find('a'+index)!=s.end() && target!='a'+index){
-                minchar = 'a' + index;
-                break;
+        int l=0,r=size-1;
+        int mid;
+        while(l<r){
+            mid = l + (r-l)/2;
+            if(letters[mid] <= target){
+                l = mid+1;
             }
-            index = (index+1)%26;
+            else {
+                minchar = letters[mid];
+                r = mid;
+            }
         }
-        return minchar;
+        cout<<l<<" "<<mid<<" "<<r<<endl;
+        return letters[r];
     }
 };
