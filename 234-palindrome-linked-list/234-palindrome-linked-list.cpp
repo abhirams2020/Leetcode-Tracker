85a636d1177ddab390969df087c7fc5d50d7ffe7
Abhirams2020 <58247649+Abhirams2020@users.noreply.github.com>
2022-03-17 07:22:25 +0530

    Time: 378 ms (30.92%), Space: 114.1 MB (72.41%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -10,23 +10,63 @@
  */
 class Solution {
 public:
+    // // array solution
+    // bool isPalindrome(ListNode* head) {
+    //     if(!head){
+    //         return true;
+    //     }
+    //     vector<int> arr;
+    //     ListNode* temp = head;
+    //     while(temp){
+    //         arr.push_back(temp->val);
+    //         temp = temp->next;
+    //     }
+    //     int l = 0, r=arr.size()-1;
+    //     while(l<r){
+    //         if(arr[l]!=arr[r]){
+    //             return false;
+    //         }
+    //         l++;
+    //         r--;
+    //     }
+    //     return true;
+    // }
+    
+    ListNode* reverseLL(ListNode* head){
+        if(!head)
+            return head;
+        ListNode* prv = NULL, *cur = head, *nxt = head->next;
+        while(cur){
+            cur->next = prv;
+            prv = cur;
+            cur = nxt;
+            if(nxt)
+                nxt = nxt->next;
+        }
+        return prv;
+    }
+    
     bool isPalindrome(ListNode* head) {
         if(!head){
             return true;
         }
-        vector<int> arr;
-        ListNode* temp = head;
-        while(temp){
-            arr.push_back(temp->val);
-            temp = temp->next;
+        ListNode *slow = head, *fast = head;
+        ListNode* beforeMid = new ListNode(0);
+        beforeMid->next = slow;
+        while(fast && fast->next){
+            beforeMid = beforeMid->next;
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        int l = 0, r=arr.size()-1;
-        while(l<r){
-            if(arr[l]!=arr[r]){
+        beforeMid->next = NULL;
+        ListNode *mid = slow;
+        ListNode *midhead = reverseLL(mid);
+        while(midhead && head){
+            if(midhead->val != head->val){
                 return false;
             }
-            l++;
-            r--;
+            midhead = midhead->next;
+            head = head->next;
         }
         return true;
     }
