550ac9986de05d118b63bbae1a57bb071066af64
Abhirams2020 <58247649+Abhirams2020@users.noreply.github.com>
2022-03-20 15:24:30 +0530

    Time: 145 ms (74.37%), Space: 85.2 MB (91.38%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
+    // vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+    //     vector<vector<int>> mat;
+    //     if(m*n != original.size())
+    //         return mat;
+    //     for(int i=0;i<m;i++){
+    //         vector<int> temp;
+    //         for(int j=0;j<n;j++){
+    //             temp.push_back(original[i*n + j]);
+    //         }
+    //         mat.push_back(temp);
+    //     }
+    //     return mat;
+    // }
+    
     vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
-        vector<vector<int>> mat;
         if(m*n != original.size())
-            return mat;
+            return {};        
+        vector<vector<int>> mat(m, vector<int>(n));
         for(int i=0;i<m;i++){
-            vector<int> temp;
             for(int j=0;j<n;j++){
-                temp.push_back(original[i*n + j]);
+                mat[i][j] = original[n*i + j];
             }
-            mat.push_back(temp);
         }
         return mat;
     }
