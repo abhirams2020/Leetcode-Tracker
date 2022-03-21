5b7d27cc7f60ee8cf7962f48b55126baeb2cf848
Abhirams2020 <58247649+Abhirams2020@users.noreply.github.com>
2022-03-21 09:44:53 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (68.28%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -1,29 +1,74 @@
 class Solution {
 public:
-    bool backspaceCompare(string s1, string s2) {
-        string res1, res2;
-        int i=0;
-        while(i<s1.length()){
-            if(!res1.empty() && s1[i]=='#'){
-                res1.pop_back();
-            }
-            else if(s1[i]!='#'){
-                res1.push_back(s1[i]);
-            }
-            i++;
-        }
-        i=0;
-        while(i<s2.length()){
-            if(!res2.empty() && s2[i]=='#'){
-                res2.pop_back();
+//     bool backspaceCompare(string s1, string s2) {
+//         string res1, res2;
+//         int i=0;
+//         while(i<s1.length()){
+//             if(!res1.empty() && s1[i]=='#'){
+//                 res1.pop_back();
+//             }
+//             else if(s1[i]!='#'){
+//                 res1.push_back(s1[i]);
+//             }
+//             i++;
+//         }
+//         i=0;
+//         while(i<s2.length()){
+//             if(!res2.empty() && s2[i]=='#'){
+//                 res2.pop_back();
                 
+//             }
+//             else if(s2[i]!='#'){
+//                 res2.push_back(s2[i]);
+//             }
+//             i++;
+//         }
+//         cout<<res1<<" "<<res2<<endl;
+//         return res1==res2;
+//     }
+
+    bool backspaceCompare(string s1, string s2) {
+        int r1=0,r2=0;
+        int curr=0;
+        while(curr<s1.length()){
+            if(r1!=0 && s1[curr]=='#'){
+                r1--;
             }
-            else if(s2[i]!='#'){
-                res2.push_back(s2[i]);
+            else if(s1[curr]!='#'){
+                s1[r1] = s1[curr];
+                r1++;
             }
-            i++;
+            curr++;
         }
-        cout<<res1<<" "<<res2<<endl;
-        return res1==res2;
+        curr=0;
+        while(curr<s2.length()){
+            if(r2!=0 && s2[curr]=='#'){
+                r2--;
+            }
+            else if(s2[curr]!='#'){
+                s2[r2] = s2[curr];
+                r2++;
+            }
+            curr++;
+        }
+        // print string
+        for(int i=0;i<r1;i++){
+            cout<<s1[i]<<" ";
+        }
+        cout<<endl;
+        for(int i=0;i<r2;i++){
+            cout<<s2[i]<<" ";
+        }
+        // check if both are equal
+        
+        if(r1!= r2){
+            return false;
+        }
+        for(int i=0, j=0; i<r1,j<r2; i++,j++){
+            if(s1[i]!=s2[j]){
+                return false;
+            }
+        }
+        return true;
     }
 };
