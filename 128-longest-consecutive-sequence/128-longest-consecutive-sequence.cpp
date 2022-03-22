7d2668d984fde97aefecdc8f441c8b2cc78103e6
abhirams2020 <58247649+abhirams2020@users.noreply.github.com>
2022-03-22 18:14:55 +0530

    Time: 95 ms (67.51%), Space: 31.7 MB (20.27%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -8,14 +8,14 @@ public:
         int count = 1;
         int maxcount = 0;
         int prev = INT_MIN/2;
-        for(auto i:s){
-            if(i-prev==1){
+        for(auto curr:s){
+            if(curr-prev==1){
                 count++;
             }
             else {
                 count=1;
             }
-            prev = i;
+            prev = curr;
             maxcount = max(maxcount, count);
         }
         return maxcount;
