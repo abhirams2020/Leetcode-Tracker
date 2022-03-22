a129e89fea107ec6d66d8144400c078d56ccd71a
abhirams2020 <58247649+abhirams2020@users.noreply.github.com>
2022-03-23 00:34:48 +0530

    Time: 4 ms (91.61%), Space: 9.3 MB (95.79%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.cpp
+++ 784-letter-case-permutation/784-letter-case-permutation.cpp
@@ -1,23 +1,45 @@
 class Solution {
 public:
+//     void permute(string ip,string op,vector<string>& ans){
+//         if(ip.length()==0){
+//             ans.push_back(op);
+//             return;
+//         }
+//         if(isalpha(ip[0])){
+//             permute(ip.substr(1),op+(char)tolower(ip[0]),ans);
+//             permute(ip.substr(1),op+(char)toupper(ip[0]),ans);
+//         }
+//         else {
+//             permute(ip.substr(1),op+ip[0],ans);
+//         }
+//     }
     
-    void permute(string ip,string op,vector<string>& ans){
-        if(ip.length()==0){
-            ans.push_back(op);
+//     vector<string> letterCasePermutation(string s) {
+//         vector<string> ans;
+//         string op;
+//         permute(s,op,ans);
+//         return ans;
+//     }
+
+    void permute(string& s,int index,vector<string>& ans){
+        if(index==s.length()){
+            ans.push_back(s);
             return;
         }
-        if(isalpha(ip[0])){
-            permute(ip.substr(1),op+(char)tolower(ip[0]),ans);
-            permute(ip.substr(1),op+(char)toupper(ip[0]),ans);
+        if(isalpha(s[index])){
+            s[index] = tolower(s[index]);
+            permute(s,index+1,ans);
+            s[index] = toupper(s[index]);
+            permute(s,index+1,ans);
         }
         else {
-            permute(ip.substr(1),op+ip[0],ans);
+            permute(s,index+1,ans);
         }
     }
+    
     vector<string> letterCasePermutation(string s) {
         vector<string> ans;
-        string op;
-        permute(s,op,ans);
+        permute(s,0,ans);
         return ans;
     }
 };
