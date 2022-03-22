e42fe26b2c6b48fe7c2c688680b5c55fa7e03301
Abhirams2020 <58247649+Abhirams2020@users.noreply.github.com>
2022-03-22 06:45:51 +0530

    Time: 94 ms (45.14%), Space: 33.5 MB (93.41%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
@@ -1,13 +1,27 @@
 class Solution {
 public:
+    // // O(N) space
+    // vector<int> findDuplicates(vector<int>& nums) {
+    //     set<int> s;
+    //     vector<int> res;
+    //     for(auto i:nums){
+    //         if(s.count(i)){
+    //             res.push_back(i);
+    //         }
+    //         s.insert(i);
+    //     }
+    //     return res;
+    // }
+    
+    // O(1) space
+    
     vector<int> findDuplicates(vector<int>& nums) {
-        set<int> s;
         vector<int> res;
         for(auto i:nums){
-            if(s.count(i)){
-                res.push_back(i);
+            if(nums[abs(i)-1] < 0){
+                res.push_back(abs(i));
             }
-            s.insert(i);
+            nums[abs(i)-1] = -nums[abs(i)-1];
         }
         return res;
     }
