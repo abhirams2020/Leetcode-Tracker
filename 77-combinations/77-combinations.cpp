0af69c06a87ef26c78e973076a5795e20975aa13
abhirams2020 <58247649+abhirams2020@users.noreply.github.com>
2022-03-23 19:46:18 +0530

    Time: 573 ms (16.27%), Space: 165 MB (9.38%) - LeetHub

---
+++ 77-combinations/77-combinations.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    
+    void solve(int n, int curr, int k, vector<int> op){
+        if(op.size()==k){
+            res.push_back(op);
+            return;
+        }
+        if(curr>n){
+            return;
+        }
+        for(int i=curr;i<=n;i++){
+            op.push_back(i);
+            solve(n, i+1, k, op);
+            op.pop_back();
+        }
+        // solve(n, curr+1, k, op);
+        // op.push_back(curr);
+        // solve(n, curr+1, k, op);
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> op;
+        solve(n,1,k,op);
+        return res;
+    }
+};
