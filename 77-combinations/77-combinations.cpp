07bac6ef9f733ccc86adc7c750e8f1a9076afd52
abhirams2020 <58247649+abhirams2020@users.noreply.github.com>
2022-03-23 20:02:06 +0530

    Time: 92 ms (25.19%), Space: 18.9 MB (28.82%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -8,22 +8,37 @@ public:
             return;
         }
         if(n-curr+1<k-op.size()){
-            cout<<curr<<" "<<op.size()<<endl;
             return;
         }
         if(curr>n){
             return;
         }
         
-        // for(int i=curr;i<=n;i++){
-        //     op.push_back(i);
-        //     solve(n, i+1, k, op);
-        //     op.pop_back();
-        // }
-        solve(n, curr+1, k, op);
-        op.push_back(curr);
-        solve(n, curr+1, k, op);
+        for(int i=curr;i<=n;i++){
+            op.push_back(i);
+            solve(n, i+1, k, op);
+            op.pop_back();
+        }
     }
+    
+//     void solve(int n, int curr, int k, vector<int> op){
+//         if(op.size()==k){
+//             res.push_back(op);
+//             return;
+//         }
+//         if(n-curr+1<k-op.size()){
+//             cout<<curr<<" "<<op.size()<<endl;
+//             return;
+//         }
+//         if(curr>n){
+//             return;
+//         }
+
+//         solve(n, curr+1, k, op);
+//         op.push_back(curr);
+//         solve(n, curr+1, k, op);
+//     }
+    
     vector<vector<int>> combine(int n, int k) {
         vector<int> op;
         solve(n,1,k,op);
