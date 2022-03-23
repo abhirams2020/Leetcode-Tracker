a280c656e83f820f28673483b58e339df540c709
abhirams2020 <58247649+abhirams2020@users.noreply.github.com>
2022-03-23 16:38:19 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (98.56%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,19 +1,60 @@
 class Solution {
 public:
     
-    int fact(int n){
-        if(n==0 || n==1){
-            return 1;
+    void solve(vector<int>&nums, int start, vector<vector<int>> &res){
+        if(start==nums.size()){
+            res.push_back(nums);
+            return;
         }
-        return n * fact(n-1);
+        
+        for(int i=start;i<nums.size();i++){
+            swap(nums[i],nums[start]);
+            solve(nums,start+1,res);
+            swap(nums[i],nums[start]);
+        }
     }
-    
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> res;
-        for(int i=0;i<fact(nums.size());i++){
-            next_permutation(nums.begin(),nums.end());
-            res.push_back(nums);
-        }
+        solve(nums,0,res);
         return res;
     }
 };
+
+// class Solution {
+// public:
+    
+//     // permute num[begin..end]
+//     // invariant: num[0..begin-1] have been fixed/permuted
+// // 	void permuteRecursive(vector<int> &num, int begin, vector<vector<int> > &result)	{
+// // 		if (begin == num.size()) {
+// // 		    // one permutation instance
+// // 		    result.push_back(num);
+// // 		    return;
+// // 		}
+		
+// // 		for (int i = begin; i < num.size(); i++) {
+// // 		    swap(num[begin], num[i]);
+// // 		    permuteRecursive(num, begin + 1, result);
+// // 		    swap(num[begin], num[i]);
+// // 		}
+// //     }
+//     void permuteRecursive(vector<int>&nums, int start, vector<vector<int>> &res){
+//         if(start>=nums.size()){
+//             res.push_back(nums);
+//             return;
+//         }
+        
+//         for(int i=start;i<nums.size();i++){
+//             swap(nums[start],nums[i]);
+//             permuteRecursive(nums,start+1,res);
+//             swap(nums[start],nums[i]);
+//         }
+//     }
+    
+//     vector<vector<int> > permute(vector<int> &num) {
+// 	    vector<vector<int>> result;
+	    
+// 	    permuteRecursive(num, 0, result);
+// 	    return result;
+//     }
+// };
