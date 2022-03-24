70df9b69fb2325a6c0163ec4ecb6cb0197998891
abhirams2020 <58247649+abhirams2020@users.noreply.github.com>
2022-03-24 19:16:32 +0530

    Time: 26 ms (33.96%), Space: 13.2 MB (40.92%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,3 +1,33 @@
+// class Solution {
+// public:
+//     vector<vector<int>> res;
+    
+//     void solve(vector<int> &candidates, int index, vector<int> op, int target){
+//         if(target==0){
+//             res.push_back(op);
+//             return;
+//         }
+//         if(index==candidates.size()){
+//             return;
+//         }
+//         if(candidates[index] <= target){
+//             solve(candidates,index+1,op,target);
+//             op.push_back(candidates[index]);
+//             solve(candidates,index,op,target-candidates[index]);
+//         }
+//         // else{
+//         //     solve(candidates,index+1,op,target);
+//         // }
+//     }
+    
+//     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+//         vector<int> op;
+//         sort(candidates.begin(),candidates.end());
+//         solve(candidates,0,op,target);
+//         return res;
+//     }
+// };
+
 class Solution {
 public:
     vector<vector<int>> res;
@@ -7,13 +37,13 @@ public:
             res.push_back(op);
             return;
         }
-        if(index==candidates.size()){
+        if(index==candidates.size() || target<candidates[index]){
             return;
         }
-        if(candidates[index] <= target){
-            solve(candidates,index+1,op,target);
-            op.push_back(candidates[index]);
-            solve(candidates,index,op,target-candidates[index]);
+        for(int i=index;i<candidates.size();i++){
+            op.push_back(candidates[i]);
+            solve(candidates,i,op,target-candidates[i]);
+            op.pop_back();
         }
         // else{
         //     solve(candidates,index+1,op,target);
