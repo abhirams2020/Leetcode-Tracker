d32e6a090ff55736450c25b7593ec3c110231375
abhirams2020 <58247649+abhirams2020@users.noreply.github.com>
2022-03-25 15:08:42 +0530

    Time: 12 ms (21.10%), Space: 18.4 MB (8.66%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    
+    vector<string> res;
+    
+    void solve(int n, int count, string op){
+        if(n==0){
+            if(count==0)
+                res.push_back(op);
+            return;
+        }
+        solve(n-1,count+1,op+'(');
+        if(count){
+            solve(n-1,count-1,op+')');
+        }
+    }
+    
+    vector<string> generateParenthesis(int n) {
+        string op;
+        solve(2*n,0,op);
+        return res;
+    }
+};
