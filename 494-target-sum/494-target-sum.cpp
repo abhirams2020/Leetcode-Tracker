bd8736500b85d98067c8af01c34d9d5b423d5883
abhirams2020 <58247649+abhirams2020@users.noreply.github.com>
2022-03-25 15:48:45 +0530

    Time: 14 ms (74.21%), Space: 9.2 MB (60.92%) - LeetHub

---
+++ 494-target-sum/494-target-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    
+    int dp[21][3000];
+    
+    int solve(vector<int> &nums, int sum, int target, int index){
+        if(sum == target and index == nums.size()){
+            return 1;
+        }
+        if(index == nums.size()){
+            return 0;
+        }
+        if(dp[index][2000+sum]!=-1){
+            return dp[index][2000+sum];
+        }
+        return dp[index][2000+sum] = solve(nums,sum-nums[index],target,index+1) + solve(nums,sum+nums[index],target,index+1);
+    }
+    
+    int findTargetSumWays(vector<int>& nums, int target) {
+        memset(dp,-1,sizeof(dp));
+        int ans = solve(nums,0,target,0);
+        return ans;
+    }
+};
