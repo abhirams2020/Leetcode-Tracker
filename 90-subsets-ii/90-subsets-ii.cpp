3d3e9a61aa3ec803fe245a237db2e1dd34496dca
abhirams2020 <58247649+abhirams2020@users.noreply.github.com>
2022-03-27 16:54:55 +0530

    Time: 4 ms (69.54%), Space: 7.8 MB (49.94%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
-    void solve(vector<int> &nums,vector<int> op,int index,set<vector<int>> &res){
-        res.insert(op);
+    void solve(vector<int> &nums,vector<int> op,int index,vector<vector<int>> &ans){
+        ans.push_back(op);
         for(int i=index;i<nums.size();i++){
             if(i==index || nums[i]!=nums[i-1]){
                 op.push_back(nums[i]);
-                solve(nums,op,i+1,res);
+                solve(nums,op,i+1,ans);
                 op.pop_back();                 
             }
         }
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        set<vector<int>> res;
         sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
         vector<int> op;
-        solve(nums,op,0,res);
-        
-        for(auto i:res){
-            ans.push_back(i);
-        }
+        solve(nums,op,0,ans);
         return ans;
     }
 };
