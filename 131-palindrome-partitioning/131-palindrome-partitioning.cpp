fecb74abdc56263efd9f56ddd1194a007ea4cf53
abhirams2020 <58247649+abhirams2020@users.noreply.github.com>
2022-03-30 11:11:20 +0530

    Time: 221 ms (37.43%), Space: 84 MB (31.19%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,36 +1,77 @@
 class Solution {
 public:
+//     vector<vector<string>> res;
+    
+//     bool checkPal(string str){
+//         int left = 0, right = str.length()-1;
+//         while(left<right){
+//             if(str[left]!=str[right]){
+//                 return false;
+//             }
+//             left++;
+//             right--;
+//         }
+//         return true;
+//     }
+    
+//     void solve(string str, vector<string> &op){
+//         if(str==""){
+//             res.push_back(op);
+//             return;
+//         }
+//         for(int i=0;i<str.length();i++){
+//             if(checkPal(str.substr(0,i+1))){
+//                 op.push_back(str.substr(0,i+1));
+//                 solve(str.substr(i+1),op);
+//                 op.pop_back();
+//             }
+//         }
+//     }
+    
+//     vector<vector<string>> partition(string s) {
+//         vector<string> op;
+//         solve(s,op);
+//         return res;
+//     }
+    
     vector<vector<string>> res;
     
-    bool checkPal(string str){
-        int left = 0, right = str.length()-1;
+    int dp[16][16];
+    
+    bool checkPal(string str, int left, int right){
+        if(dp[left][right] != -1){
+            return dp[left][right];
+        }
         while(left<right){
             if(str[left]!=str[right]){
+                dp[left][right] = 0;
                 return false;
             }
             left++;
             right--;
         }
+        dp[left][right] = 1;
         return true;
     }
     
-    void solve(string str, vector<string> &op){
-        if(str==""){
+    void solve(string str, int index, vector<string> &op){
+        if(index==str.length()){
             res.push_back(op);
             return;
         }
-        for(int i=0;i<str.length();i++){
-            if(checkPal(str.substr(0,i+1))){
-                op.push_back(str.substr(0,i+1));
-                solve(str.substr(i+1),op);
+        for(int i=index;i<str.length();i++){
+            if(checkPal(str,index,i)){
+                op.push_back(str.substr(index,i-index+1));
+                solve(str,i+1,op);
                 op.pop_back();
             }
         }
     }
     
     vector<vector<string>> partition(string s) {
+        memset(dp,-1,sizeof(dp));
         vector<string> op;
-        solve(s,op);
+        solve(s,0,op);
         return res;
     }
 };
