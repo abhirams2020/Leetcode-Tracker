72c6154d0ca577a8147cc59344c9134d74d3f43f
abhirams2020 <58247649+abhirams2020@users.noreply.github.com>
2022-03-30 15:19:35 +0530

    Time: 4 ms (34.75%), Space: 7.8 MB (47.71%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -2,6 +2,20 @@ class Solution {
 public:
     
     int dp[100];
+    // int solve(vector<int> &nums, int index){
+    //     if(index>=nums.size()){
+    //         return 0;
+    //     }
+    //     if(dp[index]!=-1){
+    //         return dp[index];
+    //     }
+    //     int mx = nums[index];
+    //     for(int i=index;i<nums.size();i++){
+    //         mx = max(mx,nums[i] + solve(nums,i+2));
+    //     }
+    //     return dp[index]=mx;
+    // }
+    
     int solve(vector<int> &nums, int index){
         if(index>=nums.size()){
             return 0;
@@ -9,11 +23,7 @@ public:
         if(dp[index]!=-1){
             return dp[index];
         }
-        int mx = nums[index];
-        for(int i=index;i<nums.size();i++){
-            mx = max(mx,nums[i] + solve(nums,i+2));
-        }
-        return dp[index]=mx;
+        return dp[index] = max(solve(nums,index+1) ,nums[index] + solve(nums,index+2));
     }
     
     int rob(vector<int>& nums) {
