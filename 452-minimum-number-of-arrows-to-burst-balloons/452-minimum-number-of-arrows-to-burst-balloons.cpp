b6668e70e60709253ee4f0de4aa4eb8a6e54d99b
abhirams2020 <58247649+abhirams2020@users.noreply.github.com>
2022-04-06 11:54:56 +0530

    Time: 444 ms (84.94%), Space: 89.8 MB (67.56%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,19 +1,23 @@
+bool cmp(vector<int> &a, vector<int> &b){
+    return a[1] < b[1];
+}
 class Solution {
 public:
     
+    // bool cmp(vector<int> &a, vector<int> &b){
+    //     return a[1] < b[1];
+    // }
+    
     int findMinArrowShots(vector<vector<int>>& points) {
-        sort(points.begin(),points.end());
+        sort(points.begin(),points.end(),cmp);
         // minEnd is the last y index encountered so far.
         // only if new pair has y greater than minEnd, we can consider it.
-        int ans = 0;
-        int minEnd = INT_MAX;
-        for(int i=0;i<points.size();i++){
-            if(ans == 0 || minEnd<points[i][0]){
+        int ans = 1;
+        int minEnd = points[0][1];
+        for(int i=1;i<points.size();i++){
+            if(minEnd<points[i][0]){
                 ans += 1;
                 minEnd = points[i][1];
-            }
-            else {
-                minEnd = min(minEnd,points[i][1]);
             }
         }
         return ans;
