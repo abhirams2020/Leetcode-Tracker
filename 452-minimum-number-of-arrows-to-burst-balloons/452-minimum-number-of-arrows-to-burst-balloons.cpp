a9ba91d80b1b7556ee95a5ed7b098d0da08599d9
abhirams2020 <58247649+abhirams2020@users.noreply.github.com>
2022-04-06 11:58:10 +0530

    Time: 1009 ms (5.04%), Space: 89.7 MB (67.56%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,8 +1,36 @@
+// class Solution {
+// public:
+    
+//     int findMinArrowShots(vector<vector<int>>& points) {
+//         sort(points.begin(),points.end());
+//         // minEnd is the last y index encountered so far.
+//         // only if new pair has y greater than minEnd, we can consider it.
+//         int ans = 1;
+//         int minEnd = points[0][1];
+//         for(int i=1;i<points.size();i++){
+//             if(minEnd<points[i][0]){
+//                 ans += 1;
+//                 minEnd = points[i][1];
+//             }
+//             else {
+//                 minEnd = min(minEnd,points[i][1]);
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
+// compare function shows error if declared inside class
+bool cmp(vector<int> &a, vector<int> &b){
+    return a[1] < b[1];
+}
+
 class Solution {
 public:
     
     int findMinArrowShots(vector<vector<int>>& points) {
-        sort(points.begin(),points.end());
+        sort(points.begin(),points.end(),cmp);
         // minEnd is the last y index encountered so far.
         // only if new pair has y greater than minEnd, we can consider it.
         int ans = 1;
@@ -11,9 +39,6 @@ public:
             if(minEnd<points[i][0]){
                 ans += 1;
                 minEnd = points[i][1];
-            }
-            else {
-                minEnd = min(minEnd,points[i][1]);
             }
         }
         return ans;
