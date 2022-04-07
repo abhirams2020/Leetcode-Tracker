d06402f43f68f069025fca93bf0e717027091aa2
abhirams2020 <58247649+abhirams2020@users.noreply.github.com>
2022-04-07 18:32:21 +0530

    Time: 160 ms (87.32%), Space: 83 MB (41.24%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -3,28 +3,28 @@ public:
     int firstMissingPositive(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         
-        int min = INT_MAX;
-        for(auto i:nums){
-            if(i>0){
-                min = i;
+        int ind = 0;
+        for(;ind<nums.size();ind++){
+            if(nums[ind]>0){
                 break;
             }
         }
-        if(min!=1)
+        if(ind==nums.size() || nums[ind]!=1) // no +ve number OR 1st positive not 1
             return 1;
         
-        int ind = 0;
         int ans = -1;
         while(ind < nums.size()-1){
-            if(nums[ind]>=0 && nums[ind+1]!=nums[ind]+1 && nums[ind]!=nums[ind+1]){
+            if(nums[ind]!=nums[ind+1] && nums[ind+1]!=nums[ind]+1){
                 ans = nums[ind]+1;
                 break;
             }
             ind++;
         }
+        
         if(ans == -1){
-            ans = nums[nums.size()-1]+1;
+            return nums[nums.size()-1]+1; //eg. arr = 0,1,2,3, then res is 4
         }
+        
         return ans;
     }
 };
