13c9ab1061cc429da7e1304ecb2e33224abb3edc
abhirams2020 <58247649+abhirams2020@users.noreply.github.com>
2022-04-07 21:21:12 +0530

    Time: 56 ms (9.35%), Space: 26.4 MB (5.08%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    int dp[3][3];
     bool check(vector<vector<char>>& board, int row, int col){
 
         if(row==board.size()-1 && col==board.size()-1){
@@ -29,20 +30,33 @@ public:
         s.clear();
         int gridRow = 3*(row/3);
         int gridCol = 3*(col/3);
-        for(int i=gridRow; i<gridRow+3;i++){
-            for(int j=gridCol; j<gridCol+3; j++){
-                if(s.count(board[i][j])){
-                    cout<<i<<" "<<j;
-                    return false;
-                }
-                if(board[i][j]!='.')
-                    s.insert(board[i][j]);
+        int dpRow = row/3, dpCol = col/3;
+        if(dp[dpRow][dpCol]!=-1){
+            if(dp[dpRow][dpCol]==0){
+                return false;
             }
         }
+        else{
+            for(int i=gridRow; i<gridRow+3;i++){
+                for(int j=gridCol; j<gridCol+3; j++){
+                    if(s.count(board[i][j])){
+                        cout<<i<<" "<<j;
+                        dp[dpRow][dpCol] = 0;
+                        return false;
+                    }
+                    if(board[i][j]!='.')
+                        s.insert(board[i][j]);
+                }
+            }
+            dp[dpRow][dpCol] = 1;
+        }
+
+        
         return check(board, row, col+1);
     }
     
     bool isValidSudoku(vector<vector<char>>& board) {
+        memset(dp,-1,sizeof(dp));
         return check(board,0,0);
     }
 };
