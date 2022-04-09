12b9628bd886749a2956a704b935b60951777472
abhirams2020 <58247649+abhirams2020@users.noreply.github.com>
2022-04-09 21:20:12 +0530

    Time: 3 ms (56.87%), Space: 6.7 MB (32.19%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,6 +1,33 @@
 class Solution {
 public:
-    map<int,int> dp;
+//     map<int,int> dp;
+    
+//     int solve(vector<int>& nums, int target){
+//         if(target<0){
+//             return 0;
+//         }
+//         if(target == 0){
+//             return 1;
+//         }
+//         if(dp.count(target)){
+//             return dp[target];
+//         }
+//         int ans = 0;
+//         for(int i=0;i<nums.size();i++){
+//             if(nums[i]>target){
+//                 break;
+//             }
+//             ans += solve(nums, target-nums[i]);
+//         }
+//         return dp[target] = ans;
+//     }
+    
+//     int combinationSum4(vector<int>& nums, int target) {
+//         sort(nums.begin(),nums.end());
+//         return solve(nums,target);
+//     }
+    
+    int dp[1001];
     
     int solve(vector<int>& nums, int target){
         if(target<0){
@@ -9,7 +36,7 @@ public:
         if(target == 0){
             return 1;
         }
-        if(dp.count(target)){
+        if(dp[target]!=-1){
             return dp[target];
         }
         int ans = 0;
@@ -23,6 +50,7 @@ public:
     }
     
     int combinationSum4(vector<int>& nums, int target) {
+        memset(dp,-1,sizeof(dp));
         sort(nums.begin(),nums.end());
         return solve(nums,target);
     }
