5e690dff8a94452c6cc58d33a2047d624325b75a
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-04-10 15:34:10 +0530

    Time: 8 ms (30.74%), Space: 8.4 MB (69.92%) - LeetHub

--- 682-baseball-game/682-baseball-game.cpp
+++ 682-baseball-game/682-baseball-game.cpp
@@ -1,39 +1,41 @@
 class Solution {
 public:
     
-    int tostring(string s){
-        int i=0;
-        int ans = 0;
-        int sign = 1;
-        if(s[0]=='-'){
-            sign = -1;
-            i = 1;
-        }
-        while(i<s.length()){
-            ans = ans*10 + (s[i]-'0');
-            i++;
-        }
-        return sign*ans;
-    }
+    // int tostring(string s){
+    //     int i=0;
+    //     int ans = 0;
+    //     int sign = 1;
+    //     if(s[0]=='-'){
+    //         sign = -1;
+    //         i = 1;
+    //     }
+    //     while(i<s.length()){
+    //         ans = ans*10 + (s[i]-'0');
+    //         i++;
+    //     }
+    //     return sign*ans;
+    // }
     
     int calPoints(vector<string>& ops) {
         vector<int> ans;
         int len = 0;
         for(auto i:ops){
-            if(i[0]=='C'){
+            if(i=="C"){
                 ans.pop_back();
                 len--;
             }
-            else if(i[0]=='D'){
+            else if(i=="D"){
                 ans.push_back(2*ans[ans.size()-1]);
                 len++;
             }
-            else if(i[0]=='+'){
+            else if(i=="+"){
                 ans.push_back(ans[ans.size()-1] + ans[ans.size()-2]);
                 len++;
             }
             else {
-                ans.push_back(tostring(i));
+                // ans.push_back(tostring(i));
+                // use stoi fn to convert string to integer
+                ans.push_back(stoi(i));
                 len++;
             }
         }
