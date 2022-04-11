289e5de1477acca602c201a69298fdc3363d381c
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-04-12 00:35:27 +0530

    Time: 43 ms (50.14%), Space: 13.8 MB (97.44%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
-   vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+    // consider 2d m*n array as 1d array
+    // we can represent arr[i][j] as n*i + j
+    // shift k times means n*i+j+k. %m if >m
+
+    // eg [1][2] to [2][0] (shift by 1) in 4*3 grid
+    // 0 -> (2+1)%3 %m if j>n-1
+    // 2 -> (3*1+2+1)/3 = 1+(2+1)/3 = 6/3 = 2. %m if i>m-1
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
        int m = grid.size();
        int n = grid[0].size();
        int r=0,c=0;
@@ -8,7 +15,7 @@ public:
        for(int i=0;i<m;i++){
            for(int j=0;j<n;j++){
                c = (j+k)%n;
-               r = ((n*i+j+k)/n)%m;
+               r = (i+(j+k)/n)%m;
                ans[r][c] = grid[i][j];
            }
        }
