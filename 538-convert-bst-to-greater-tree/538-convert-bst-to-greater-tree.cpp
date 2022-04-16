f255b0d5e86628ce79c70454b8245c968dd3efad
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-04-16 23:23:07 +0530

    Time: 48 ms (63.86%), Space: 33.4 MB (93.89%) - LeetHub

--- 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
@@ -15,21 +15,42 @@ public:
         use reverse inorder to calculate the sum greater than or equal to current
         after each step, increase sum and change root->val to sum
     */
+//     int sum = 0;
+//     void revinorder(TreeNode* root){
+//         if(!root){
+//             return;
+//         }
+//         revinorder(root->right);
+    
+//         sum+=root->val;
+//         root->val = sum;
+    
+//         revinorder(root->left);
+//     }
+    
+//     TreeNode* convertBST(TreeNode* root) {
+//         revinorder(root);
+//         return root;
+//     }
+    
+    /*  
+        use reverse inorder to calculate the sum greater than or equal to current
+        after each step, increase sum and change root->val to sum
+        
+        wITHOUT HELPER FUNCTION
+    */    
     int sum = 0;
-    void revinorder(TreeNode* root){
-        if(!root){
-            return;
-        }
-        revinorder(root->right);
-        
-        sum+=root->val;
-        root->val = sum;
-        
-        revinorder(root->left);
-    }
     
     TreeNode* convertBST(TreeNode* root) {
-        revinorder(root);
+        if(!root){
+            return NULL;
+        }
+        root->right = convertBST(root->right);
+        
+        sum += root->val;
+        root->val = sum;
+        
+        root->left = convertBST(root->left);
         return root;
     }
 };
