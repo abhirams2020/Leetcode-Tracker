3d212ed7af12caf609be82e7a00fdc650a8572e1
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-04-17 23:39:48 +0530

    Time: 41 ms (68.19%), Space: 12.3 MB (57.78%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    
+    void fill(vector<vector<char>>& grid, int i, int j){
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size()){
+            return;
+        }
+        if(grid[i][j] == '0' || grid[i][j] == '2'){
+            return;
+        }
+        grid[i][j] = '2';
+        fill(grid,i+1,j);
+        fill(grid,i,j+1);
+        fill(grid,i-1,j);
+        fill(grid,i,j-1);
+    }
+    
+    int numIslands(vector<vector<char>>& grid) {
+        int count = 0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]=='1'){
+                    count++;
+                    fill(grid,i,j);
+                }
+            }
+        }
+        return count;
+    }
+};
