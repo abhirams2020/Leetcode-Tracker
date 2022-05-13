c9e6fd75420945adad46180a381207d044209c75
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-14 01:09:58 +0530

    Time: 44 ms (56.57%), Space: 26.5 MB (88.68%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -1,31 +1,74 @@
 class Solution {
 public:
+    
+    // sort the array and compare each element O(N log N)
+//     int findUnsortedSubarray(vector<int>& nums) {
+//         int n = nums.size();
+//         int start=0,end=n-1;
+//         vector<int> dup(nums.begin(), nums.end());
+        
+//         sort(dup.begin(),dup.end());
+        
+//         for(int i=0;i<n;i++){
+//             if(nums[i]!=dup[i]){
+//                 break;
+//             }
+//             start++;
+//         }
+        
+//         if(start == n){
+//             return 0;
+//         }
+        
+//         for(int i=n-1;i>=0;i--){
+//             if(nums[i]!=dup[i]){
+//                 break;
+//             }
+//             end--;
+//         }
+        
+//         if(start!=end){
+//             return end - start + 1;
+//         }
+//         return 0;
+//     }
+    
+    // O(N) time
+    
     int findUnsortedSubarray(vector<int>& nums) {
         int n = nums.size();
+        int small = INT_MAX, large = INT_MIN;
         int start=0,end=n-1;
-        vector<int> dup(nums.begin(), nums.end());
         
-        sort(dup.begin(),dup.end());
+        for(int i=0;i<n-1;i++){
+            if(nums[i]>nums[i+1]){
+                large = max(nums[i], large);
+            }
+        }
         
-        for(int i=0;i<n;i++){
-            if(nums[i]!=dup[i]){
+        for(int i=n-1;i>=1;i--){
+            if(nums[i]<nums[i-1]){
+                small = min(nums[i], small);
+            }
+        }
+        
+        for(int i=n-1;i>=1;i--){
+            if(nums[i]<large){
+                end = i;
                 break;
             }
-            start++;
         }
         
-        if(start == n){
-            return 0;
-        }
-        
-        for(int i=n-1;i>=0;i--){
-            if(nums[i]!=dup[i]){
+        for(int i=0;i<n-1;i++){
+            if(nums[i]>small){
+                start = i;
                 break;
             }
-            end--;
         }
         
-        if(start!=end){
+        cout<<small<<" "<<large<<endl;
+        cout<<start<<" "<<end<<endl;
+        if(small!=INT_MAX && start!=end){
             return end - start + 1;
         }
         return 0;
