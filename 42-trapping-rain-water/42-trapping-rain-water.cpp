55d8951ef7adaf801c089945d57239c6e2a4a04e
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-13 15:02:01 +0530

    Time: 12 ms (84.40%), Space: 17.6 MB (92.76%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,21 +1,41 @@
 class Solution {
 public:
+//     int trap(vector<int>& height) {
+//         int left=0, right=0, sum=0, n = height.size();
+//         vector<int> maxLeft(n), maxRight(n);
+        
+//         for(int i=0;i<n;i++){
+//             left = max(height[i],left);
+//             maxLeft[i] = left;
+//         }
+        
+//         for(int i=n-1; i>=0;i--){
+//             right = max(height[i],right);
+//             maxRight[i] = right;
+//         }
+        
+//         for(int i=0;i<n;i++){
+//             sum += min(maxLeft[i],maxRight[i]) - height[i];
+//         }
+        
+//         return sum;
+//     }
+    
     int trap(vector<int>& height) {
-        int left=0, right=0, sum=0, n = height.size();
-        vector<int> maxLeft(n), maxRight(n);
+        int maxLeft=0, maxRight=0, sum=0;
+        int left=0, right=height.size()-1;
         
-        for(int i=0;i<n;i++){
-            left = max(height[i],left);
-            maxLeft[i] = left;
-        }
-        
-        for(int i=n-1; i>=0;i--){
-            right = max(height[i],right);
-            maxRight[i] = right;
-        }
-        
-        for(int i=0;i<n;i++){
-            sum += min(maxLeft[i],maxRight[i]) - height[i];
+        while(left < right){
+            if(height[left] < height[right]){
+                maxLeft = max(maxLeft, height[left]);
+                sum += maxLeft - height[left];
+                left++;
+            }
+            else {
+                maxRight = max(maxRight, height[right]);
+                sum += maxRight - height[right];
+                right--;
+            }
         }
         
         return sum;
