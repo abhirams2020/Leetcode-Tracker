c141d9bd4d9de84d75993826f3c5163ea2c9b0f5
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-15 13:13:03 +0530

    Time: 10 ms (32.46%), Space: 7.2 MB (98.38%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
+    
+    // bool isPalindrome(string s) {
+    //     int l=0, r=s.length()-1;
+    //     while(l<r){
+    //         if(!isalnum(s[l])){
+    //             l++;
+    //         }
+    //         else if(!isalnum(s[r])){
+    //             r--;
+    //         }
+    //         else {
+    //             if(tolower(s[l]) != tolower(s[r])){
+    //                 return false;
+    //             }
+    //             l++;
+    //             r--;
+    //         }
+    //     }
+    //     return true;
+    // }
+    
     bool isPalindrome(string s) {
-        string str;
-        for(auto c:s){
-            if(isalnum(c))
-            str.push_back(tolower(c));
-        }
-        int left=0, right=str.length()-1;
-        while(left < right){
-            if(str[left]!=str[right]){
+        int l=0, r=s.length()-1;
+        while(l < r){
+            while(l<s.length() && !isalnum(s[l]))
+                l++;
+            while(r>0 && !isalnum(s[r]))
+                r--;
+            if(l<r && tolower(s[l])!=tolower(s[r]))
                 return false;
-            }
-            left++;
-            right--;
+            l++;
+            r--;
         }
         return true;
     }
