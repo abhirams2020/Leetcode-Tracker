99b964808f0139959ac7c31d27571689be5c00b6
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-15 12:36:56 +0530

    Time: 172 ms (45.35%), Space: 49.9 MB (5.23%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -21,33 +21,65 @@ public:
     //     return maxcount;
     // }
 
+//     int longestConsecutive(vector<int>& nums) {
+//         if(nums.size()==0){
+//             return 0;
+//         }
+//         unordered_set<int> s;
+//         for(auto i:nums){
+//             s.insert(i);
+//         }
+//         int maxcount = 1;
+//         while(!s.empty()){
+//             int val = *s.begin();
+//             s.erase(val);
+//             int count = 1;
+            
+//             // val, val+1, val+2 ...
+//             for(int i=val+1;s.count(i);i++){
+//                 s.erase(i);
+//                 count++;
+//             }
+            
+//             // val-2, val-1, val ...
+//             for(int i=val-1;s.count(i);i--){
+//                 s.erase(i);
+//                 count++;
+//             }
+//             maxcount = max(maxcount,count);
+//         }
+//         return maxcount;
+//     }
+    
     int longestConsecutive(vector<int>& nums) {
         if(nums.size()==0){
             return 0;
         }
-        unordered_set<int> s;
-        for(auto i:nums){
-            s.insert(i);
+        unordered_map<int,int> mp;
+        int maxVal = 1, n;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]] = i;
         }
-        int maxcount = 1;
-        while(!s.empty()){
-            int val = *s.begin();
-            s.erase(val);
-            int count = 1;
-            
-            // val, val+1, val+2 ...
-            for(int i=val+1;s.count(i);i++){
-                s.erase(i);
-                count++;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=INT_MAX){
+                int count = 1;
+                n = nums[i]+1;
+                while(mp.count(n)){
+                    count++;
+                    nums[mp[n]] = INT_MAX;
+                    mp.erase(n);
+                    n++;
+                }
+                n = nums[i]-1;
+                while(mp.count(n)){
+                    count++;
+                    nums[mp[n]] = INT_MAX;
+                    mp.erase(n);
+                    n--;
+                }
+                maxVal = max(maxVal, count);
             }
-            
-            // val-2, val-1, val ...
-            for(int i=val-1;s.count(i);i--){
-                s.erase(i);
-                count++;
-            }
-            maxcount = max(maxcount,count);
         }
-        return maxcount;
+        return maxVal;
     }
 };
