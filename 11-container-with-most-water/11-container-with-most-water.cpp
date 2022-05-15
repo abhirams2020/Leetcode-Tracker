871f425c0bd3c22e9fbc9a34a0d516c39671c905
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-15 16:49:21 +0530

    Time: 107 ms (71.16%), Space: 59 MB (82.18%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -5,8 +5,12 @@ public:
         int maxVol = INT_MIN;
         int l=0,r=n-1;
         while(l<r) {
+            // area is width*heighth. we take min of left and right height.
             int vol = min(height[l],height[r]) * (r-l);
             maxVol = max(maxVol, vol);
+            
+            // we move the lower height to get the max vol
+            // if both height same, the max with h is that. so move l and r.
             if(height[l]<height[r]){
                 l++;
             }
