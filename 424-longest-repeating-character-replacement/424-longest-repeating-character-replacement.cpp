2726338bf5742e545bce1f3d94bfbb01bab2a587
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-17 19:38:24 +0530

    Time: 10 ms (77.08%), Space: 7.1 MB (53.46%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        vector<int> count(256,0);
+        vector<int> count(26,0);
         int maxLen = 1;
         char maxChar = s[0];
         int l=0;
         for(int r=0;r<s.length();r++){
-            count[s[r]]++;
-            if(count[s[r]]>=count[maxChar]){
+            count[s[r] - 'A']++;
+            if(count[s[r] - 'A']>=count[maxChar - 'A']){
                 maxChar = s[r];
             }
-            while((r-l+1) - count[maxChar] > k){
-                count[s[l]]--;
-                if(count[s[r]] >= count[maxChar]){
-                    maxChar = s[r];
-                }
+            while((r-l+1) - count[maxChar - 'A'] > k){
+                count[s[l] - 'A']--;
+                
+                // we don't need loop to update maxChar since maxcount in the window is count[maxChar].
+                // when we move l to right, the maximum of counts remains less than the maxcount.
+                
+                // if(count[s[r]] >= count[maxChar]){
+                //     maxChar = s[r];
+                // }
+                
                 l++;
             }
             maxLen = max(maxLen, r-l+1);
         }
-        
         return maxLen;
     }
 };
