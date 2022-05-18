e2c19c34037c169c652aacf195933253c878285f
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-18 11:04:04 +0530

    Time: 507 ms (5.72%), Space: 141.6 MB (5.00%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    
+    bool isSubsetMap(unordered_map<char,int> mp1, unordered_map<char,int> mp2){
+        for(auto i:mp1){
+            if(mp2[i.first] != i.second){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    bool checkInclusion(string s1, string s2) {
+        unordered_map<char, int> mp1, mp2;
+        for(auto i:s1){
+            mp1[i]++;
+        }
+        int l=0, r=0;
+        for(r=0; r<s2.length(); r++) {
+            mp2[s2[r]]++;
+            if(r-l+1 == s1.length()) {
+                if(isSubsetMap(mp1,mp2)){
+                    return true;
+                }
+                mp2[s2[l]]--;
+                if(mp2[s2[l]] == 0){
+                    mp2.erase(s2[l]);
+                }
+                l++;
+            }
+        }
+        return false;
+    }
+};
