472b03ac3bc0385511b0a14e7ea7f4494ae3a561
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-18 19:22:04 +0530

    Time: 765 ms (11.28%), Space: 145.6 MB (31.57%) - LeetHub

---
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    typedef pair<int,int> pp;
+    
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> ans;
+        priority_queue<pp> pq;
+        int l=0;
+        for(int r=0;r<nums.size();r++){
+            pq.push({nums[r],r});
+            if(r-l+1==k){
+                while(pq.top().second < l){
+                    pq.pop();
+                }
+                ans.push_back(pq.top().first);
+                l++;
+            }
+        }
+        return ans;
+    }
+};
