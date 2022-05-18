cc2fa566c8907b4ad0d371a4b546c0531e27264a
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-18 23:32:39 +0530

    Time: 338 ms (68.84%), Space: 131.9 MB (50.50%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -43,7 +43,7 @@ public:
         vector<int> ans;
         deque<int> dq;
         for(int i=0;i<nums.size();i++){
-            if(!dq.empty() && dq.front() <= i-k){
+            if(!dq.empty() && dq.front() == i-k){
                 dq.pop_front();
             }
             while(!dq.empty() && nums[dq.back()] < nums[i]){
