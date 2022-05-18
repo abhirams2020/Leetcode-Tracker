e363e33391855aec47b3ad312f43eeceea0723c6
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-18 23:24:08 +0530

    Time: 458 ms (35.14%), Space: 133.8 MB (39.83%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -19,38 +19,21 @@ public:
     //     return ans;
     // }
     
-    // vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-    //     vector<int> ans;
-    //     deque<pair<int,int>> dq;
-    //     for(int i=0;i<nums.size();i++){
-    //         if(!dq.empty() && dq.front().second <= i-k){
-    //             dq.pop_front();
-    //         }
-    //         while(!dq.empty() && dq.back().first < nums[i]){
-    //             dq.pop_back();
-    //         }
-    //         dq.push_back({nums[i],i});
-    //         if(i>=k-1){
-    //             ans.push_back(dq.front().first);
-    //         }
-    //     }
-    //     return ans;
-    // }
-    
+    // USING DEQUE (OPTIMAL SOLN)
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        //Pair<int,int> contains (nums[i],index)
-        deque<pair<int,int>> dq;  //We will store elements in the deque only inside our current window
         vector<int> ans;
-        
-        for(int i=0;i<nums.size();++i)
-        {
-            if(!dq.empty() and dq.front().second<=(i-k))    //Remove front element if it goes out of window size
+        deque<pair<int,int>> dq;
+        for(int i=0;i<nums.size();i++){
+            if(!dq.empty() && dq.front().second <= i-k){
                 dq.pop_front();
-            while(!dq.empty() and dq.back().first<nums[i])  //Maintain elements in DSC order
+            }
+            while(!dq.empty() && dq.back().first < nums[i]){
                 dq.pop_back();
-            dq.push_back(make_pair(nums[i],i));     //Push current NODE
-            if(i>=(k-1))
-                ans.push_back(dq.front().first);    //Include maximum of cuurrent window
+            }
+            dq.push_back({nums[i],i});
+            if(i>=k-1){
+                ans.push_back(dq.front().first);
+            }
         }
         return ans;
     }
