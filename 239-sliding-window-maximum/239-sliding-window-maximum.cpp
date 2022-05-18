88b0f033d18473296323fc941e1c6360ee0b7f64
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-18 23:03:32 +0530

    Time: 390 ms (51.76%), Space: 133.9 MB (38.99%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,17 +1,37 @@
 class Solution {
 public:
+    
+    // USING HEAP
+    // vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+    //     vector<int> ans;
+    //     priority_queue<pair<int,int>> pq;
+    //     int l=0;
+    //     for(int r=0;r<nums.size();r++){
+    //         pq.push({nums[r],r});
+    //         if(r-l+1==k){
+    //             while(pq.top().second < l){
+    //                 pq.pop();
+    //             }
+    //             ans.push_back(pq.top().first);
+    //             l++;
+    //         }
+    //     }
+    //     return ans;
+    // }
+    
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         vector<int> ans;
-        priority_queue<pair<int,int>> pq;
-        int l=0;
-        for(int r=0;r<nums.size();r++){
-            pq.push({nums[r],r});
-            if(r-l+1==k){
-                while(pq.top().second < l){
-                    pq.pop();
-                }
-                ans.push_back(pq.top().first);
-                l++;
+        deque<pair<int,int>> dq;
+        for(int i=0;i<nums.size();i++){
+            if(!dq.empty() && dq.front().second <= i-k){
+                dq.pop_front();
+            }
+            while(!dq.empty() && dq.back().first < nums[i]){
+                dq.pop_back();
+            }
+            dq.push_back({nums[i],i});
+            if(i>=k-1){
+                ans.push_back(dq.front().first);
             }
         }
         return ans;
