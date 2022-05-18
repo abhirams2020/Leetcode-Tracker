cd40cef3e90719deb592bea688d192743c65a482
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-18 18:19:27 +0530

    Time: 14 ms (78.87%), Space: 7.8 MB (68.17%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -1,53 +1,11 @@
 class Solution {
 public:
-    
-//     // USING UNORDERED_MAP
-    
-//     string minWindow(string searchString, string t) {
-
-//         // Our hashtable of characters filled with occurences in our string
-//         // vector<int> table(128, 0);
-//         unordered_map<char,int> table;
-//         // Store the frequency of characters in string t
-//         for(auto character : t) {
-//             table[character]++;
-//         }
-
-//         int start = 0;
-//         int end = 0;
-//         int counter = t.size();
-//         int startMin = 0;
-//         int minLen = INT_MAX;
-
-//         // Here we use the 2 pointer approach
-//         while(end < searchString.size()) {
-
-//             if(table.count(searchString[end]) && table[searchString[end]] > 0) {
-//                 counter--;
-//             }
-//             table[searchString[end]]--;
-//             end++;
-
-//             while(counter == 0) {
-//                 if(end - start < minLen) {
-//                     startMin = start;
-//                     minLen = end - start;
-//                 }
-//                 table[searchString[start]]++;
-//                 if(table.count(searchString[start]) && table[searchString[start]] > 0) {
-//                     counter++;
-//                 }
-//                 start++;
-//             }
-//         }
-
-//         // Case when no substring satisfies
-//         if(minLen == INT_MAX) {
-//           return "";
-//         }
-
-//         return searchString.substr(startMin, minLen);
-//     }
+/*  
+    table[s[l]]++ is increasing the value of the characters which were reduced by table[s[r]]--. 
+    So it will return to original value in case of characters not in string t, ie 0. 
+    Characters which were in string t will get back the original values. ie. table[A = B = C] = 2. 
+    So table[s[l]] will never be +ve in case of other characters. 
+*/
     
     // USING VECTOR MAP
     
@@ -67,7 +25,7 @@ public:
         int startMin = 0;
         int minLen = INT_MAX;
 
-        // Here we use the 2 pointer approach
+        // Here we use the 2 pointer approach sliding window and rolling hash
         while(end < searchString.size()) {
 
             if(table[searchString[end]] > 0) {
@@ -81,6 +39,7 @@ public:
                     startMin = start;
                     minLen = end - start;
                 }
+    
                 table[searchString[start]]++;
                 if(table[searchString[start]] > 0) {
                     counter++;
