9812f9ed272feae667fb52d64def45291203f41b
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-18 23:04:04 +0530

    Time: 473 ms (32.31%), Space: 133.9 MB (39.83%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -19,20 +19,38 @@ public:
     //     return ans;
     // }
     
+    // vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+    //     vector<int> ans;
+    //     deque<pair<int,int>> dq;
+    //     for(int i=0;i<nums.size();i++){
+    //         if(!dq.empty() && dq.front().second <= i-k){
+    //             dq.pop_front();
+    //         }
+    //         while(!dq.empty() && dq.back().first < nums[i]){
+    //             dq.pop_back();
+    //         }
+    //         dq.push_back({nums[i],i});
+    //         if(i>=k-1){
+    //             ans.push_back(dq.front().first);
+    //         }
+    //     }
+    //     return ans;
+    // }
+    
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        //Pair<int,int> contains (nums[i],index)
+        deque<pair<int,int>> dq;  //We will store elements in the deque only inside our current window
         vector<int> ans;
-        deque<pair<int,int>> dq;
-        for(int i=0;i<nums.size();i++){
-            if(!dq.empty() && dq.front().second <= i-k){
+        
+        for(int i=0;i<nums.size();++i)
+        {
+            if(!dq.empty() and dq.front().second<=(i-k))    //Remove front element if it goes out of window size
                 dq.pop_front();
-            }
-            while(!dq.empty() && dq.back().first < nums[i]){
+            while(!dq.empty() and dq.back().first<nums[i])  //Maintain elements in DSC order
                 dq.pop_back();
-            }
-            dq.push_back({nums[i],i});
-            if(i>=k-1){
-                ans.push_back(dq.front().first);
-            }
+            dq.push_back(make_pair(nums[i],i));     //Push current NODE
+            if(i>=(k-1))
+                ans.push_back(dq.front().first);    //Include maximum of cuurrent window
         }
         return ans;
     }
