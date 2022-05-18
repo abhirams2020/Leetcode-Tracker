fbbecd2b6acaeac9893125c1218b811e1757d878
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-18 11:06:58 +0530

    Time: 14 ms (51.70%), Space: 8.1 MB (22.19%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
     
-    bool isSubsetMap(unordered_map<char,int> mp1, unordered_map<char,int> mp2){
-        for(auto i:mp1){
-            if(mp2[i.first] != i.second){
-                return false;
-            }
-        }
-        return true;
-    }
-    
     bool checkInclusion(string s1, string s2) {
         unordered_map<char, int> mp1, mp2;
         for(auto i:s1){
@@ -19,12 +10,11 @@ public:
         for(r=0; r<s2.length(); r++) {
             mp2[s2[r]]++;
             if(r-l+1 == s1.length()) {
-                // if(isSubsetMap(mp1,mp2)){
-                //     return true;
-                // }
                 if(mp1 == mp2){
                     return true;
                 }
+                
+                // move left ptr
                 mp2[s2[l]]--;
                 if(mp2[s2[l]] == 0){
                     mp2.erase(s2[l]);
