2f0fe8083465ca82119117f034145723d68dc493
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-19 13:11:09 +0530

    Time: 28 ms (10.47%), Space: 20.3 MB (64.75%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -13,7 +13,7 @@ class Solution {
 public:
     int maxDepth(TreeNode* root){
         if(!root) return 0;
-        if(!root->left && !root->right) return 1;
+        // if(!root->left && !root->right) return 1;
         return 1 + max(maxDepth(root->left), maxDepth(root->right));
     }
     
