e5ae311ad5b3095cef1c671d5bfb93558ef9459b
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-20 23:32:47 +0530

    Time: 7 ms (5.49%), Space: 6.4 MB (10.82%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string str) {
+        if(str.length()%2){
+            return false;
+        }
+        map<char,char> mp = {{')','('}, {']','[' }, {'}','{'}};
+        stack<char> st;
+        set<char> s = {'(','[','{'};
+        for(char ch:str) {
+            if(s.find(ch)!=s.end()) {
+                st.push(ch);
+            }
+            else {
+                if(st.empty() || st.top()!=mp[ch]){
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        if(st.empty()){
+            return true;
+        }
+        return false;
+    }
+};
