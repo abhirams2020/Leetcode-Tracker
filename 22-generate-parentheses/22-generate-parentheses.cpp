c0b707c88be4f010cbaec9c14f9693242e51fafc
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-21 11:05:30 +0530

    Time: 6 ms (52.75%), Space: 13.7 MB (46.63%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -2,13 +2,19 @@ class Solution {
 public:
     vector<string> ans;
     
+    // open represents number of opening brackets left, closed is number of closing bracket left
     void solve(int open, int close, string s){
+        // when no more opening and closing to be added, push to ans
         if(open==0 && close==0){
             ans.push_back(s);
             return;
         }
+        
+        // if there is opening bracket left, add (
         if(open>0)
             solve(open-1,close,s+'(');
+        
+        // if number of closing bracket remaining than no of open, add )
         if(close>open){
             solve(open,close-1,s+')');
         }
