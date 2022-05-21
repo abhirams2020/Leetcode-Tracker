cb517f7a0a89a7df6831e969c1358c3579dfe359
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-21 11:25:49 +0530

    Time: 243 ms (32.59%), Space: 85.1 MB (71.24%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.cpp
+++ 739-daily-temperatures/739-daily-temperatures.cpp
@@ -5,9 +5,13 @@ public:
         stack<int> st;
         vector<int> ans(n,0);
         for(int i=n-1;i>=0;i--){
+            // pop all elements less than or equal to element to be pushed
             while(!st.empty() && temperatures[st.top()]<=temperatures[i]){
                 st.pop();
             }
+            
+            // if stack is empty,no result so we keep original value 0
+            // if stack not empty, we can calculate the no of days
             if(!st.empty()){
                 ans[i] = st.top() - i;
             }
