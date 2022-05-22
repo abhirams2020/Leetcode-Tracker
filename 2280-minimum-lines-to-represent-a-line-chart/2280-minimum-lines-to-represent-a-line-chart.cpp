609f01e94102c1787755e01bbfbe378714c8462a
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-22 15:01:54 +0530

    Time: 718 ms (33.33%), Space: 114.6 MB (33.33%) - LeetHub

--- 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.cpp
+++ 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
+    
     int minimumLines(vector<vector<int>>& stockPrices) {
         if(stockPrices.size() <= 1)
             return 0;
-        vector<int> slopes;
+        vector<long double> slopes;
         sort(stockPrices.begin(),stockPrices.end());
+        for(int i=0;i<stockPrices.size()-1;i++){
+            long double currSlope = (long double)(stockPrices[i+1][1]-stockPrices[i][1])/(stockPrices[i+1][0]-stockPrices[i][0]);
+            slopes.push_back(currSlope);
+        }
         int count = 1;
-        for(int i=0;i<stockPrices.size()-2;i++){
-            long x1=stockPrices[i][0], x2=stockPrices[i+1][0], x3=stockPrices[i+2][0];
-            long y1=stockPrices[i][1], y2=stockPrices[i+1][1], y3=stockPrices[i+2][1];
-            if( (y2-y1)*(x3-x2) != (y3-y2)*(x2-x1) ){
+        for(int i=0;i<slopes.size()-1;i++){
+            if(slopes[i+1]!=slopes[i]){
                 count++;
             }
         }
         return count;
     }
+
+    // int minimumLines(vector<vector<int>>& stockPrices) {
+    //     if(stockPrices.size() <= 1)
+    //         return 0;
+    //     vector<int> slopes;
+    //     sort(stockPrices.begin(),stockPrices.end());
+    //     int count = 1;
+    //     for(int i=0;i<stockPrices.size()-2;i++){
+    //         long x1=stockPrices[i][0], x2=stockPrices[i+1][0], x3=stockPrices[i+2][0];
+    //         long y1=stockPrices[i][1], y2=stockPrices[i+1][1], y3=stockPrices[i+2][1];
+    //         if( (y2-y1)*(x3-x2) != (y3-y2)*(x2-x1) ){
+    //             count++;
+    //         }
+    //     }
+    //     return count;
+    // }
 };
