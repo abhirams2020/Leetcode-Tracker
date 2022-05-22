b8efe865f04547cfc730d921589b915ffd4b7769
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-23 01:01:53 +0530

    Time: 1762 ms (5.01%), Space: 287.6 MB (5.03%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>> &graph, int source, int destination, vector<int>& visited){
+    bool dfs(unordered_map<int,vector<int>>& graph, int source, int destination, unordered_set<int>& visited){
         if(source == destination){
             return true;
         }
-        visited[source] = 1;
+        visited.insert(source);
         for(auto i:graph[source]){
-            if(visited[i]==0 && dfs(graph,i,destination,visited)){
+            if(!visited.count(i) && dfs(graph,i,destination,visited)){
                 return true;
             }
         }
@@ -14,14 +14,37 @@ public:
     }
     
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        vector<vector<int>> graph(n);
+        unordered_map<int, vector<int>> graph;
         for(auto node:edges){
             graph[node[0]].push_back(node[1]);
             graph[node[1]].push_back(node[0]);
         }
-        vector<int> visited(n,0);
+        unordered_set<int> visited;
         return dfs(graph,source,destination,visited);
     }
+    
+//     bool dfs(vector<vector<int>> &graph, int source, int destination, vector<int>& visited){
+//         if(source == destination){
+//             return true;
+//         }
+//         visited[source] = 1;
+//         for(auto i:graph[source]){
+//             if(visited[i]==0 && dfs(graph,i,destination,visited)){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+    
+//     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+//         vector<vector<int>> graph(n);
+//         for(auto node:edges){
+//             graph[node[0]].push_back(node[1]);
+//             graph[node[1]].push_back(node[0]);
+//         }
+//         vector<int> visited(n,0);
+//         return dfs(graph,source,destination,visited);
+//     }
     
 //     bool validPath(int n, vector<vector<int>>& edges, int start, int end) {
 //         unordered_map<int,vector<int>> graph; 
