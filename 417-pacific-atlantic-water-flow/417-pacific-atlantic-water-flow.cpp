3ee7f34c639e6253e36a811da3c3f0bb9ecad992
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-23 16:52:13 +0530

    Time: 49 ms (73.90%), Space: 17.6 MB (73.28%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -4,22 +4,27 @@ public:
     
     void dfs(vector<vector<int>>& heights, vector<vector<bool>> &visited, int i, int j){
         int m = heights.size(), n = heights[0].size();
-        // if(i<0 || j<0 || i==m || j==n) {
-        //     return;
-        // }
         
         if(visited[i][j] == true)
             return;
         
         visited[i][j] = true;
         
-        // do dfs
+        // DO DFS
+        
+        // DOWN
         if(i+1<m && heights[i+1][j]>=heights[i][j])
             dfs(heights,visited,i+1,j);
+        
+        // UP
         if(i-1>=0 && heights[i-1][j]>=heights[i][j])
             dfs(heights,visited,i-1,j);
+        
+        // RIGHT
         if(j+1<n && heights[i][j+1]>=heights[i][j])
             dfs(heights,visited,i,j+1);
+        
+        // LEFT
         if(j-1>=0 && heights[i][j-1]>=heights[i][j])
             dfs(heights,visited,i,j-1);
     }
@@ -33,16 +38,8 @@ public:
         vector<vector<bool>> pacific(m, vector<bool> (n,false));
         vector<vector<bool>> atlantic(m, vector<bool> (n,false));
         
-        // for(int i=0;i<heights.size();i++){
-        //     for(int j=0;j<heights[0].size();j++){
-        //         if(i==0 || j==0){
-        //             dfs(heights,pacific,i,j);
-        //         }
-        //         if(i==m-1 || j==n-1){
-        //             dfs(heights,atlantic,i,j);
-        //         }
-        //     }
-        // }
+        // Start from the sides touching ocean and move inwards.
+        // Visiting a node meaning it can flow water to the ocean
         
         // iterating through first COLUMN for pacific and last for atlantic
         for(int i=0;i<m;i++){
