a376ba94480789289fa7e85198946cdd6079406d
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-23 14:50:12 +0530

    Time: 3 ms (96.81%), Space: 8.6 MB (85.20%) - LeetHub

--- 133-clone-graph/133-clone-graph.cpp
+++ 133-clone-graph/133-clone-graph.cpp
@@ -21,6 +21,30 @@ public:
 
 class Solution {
 public:
+    
+//     // USING DFS
+    
+//     // map to store new copy of an existing node
+//     // copies[old node of 1] returns new node of 1 if it exist
+//     unordered_map<Node*,Node*> copies;
+
+//     Node* cloneGraph(Node* node) {
+//         if(node==NULL){
+//             return NULL;
+//         }
+//         // if copy already has node, then we dont need to find its neighbours
+//         // if not, add all neighbours of old node to new node
+//         if(!copies.count(node)){
+//             copies[node] = new Node(node->val);
+//             for(auto i:node->neighbors){
+//                 copies[node]->neighbors.push_back(cloneGraph(i));
+//             }
+//         }
+//         return copies[node];
+//     }
+    
+    // USING BFS
+    
     // map to store new copy of an existing node
     // copies[old node of 1] returns new node of 1 if it exist
     unordered_map<Node*,Node*> copies;
@@ -29,12 +53,18 @@ public:
         if(node==NULL){
             return NULL;
         }
-        // if copy already has node, then we dont need to find its neighbours
-        // if not, add all neighbours of old node to new node
-        if(!copies.count(node)){
-            copies[node] = new Node(node->val);
-            for(auto i:node->neighbors){
-                copies[node]->neighbors.push_back(cloneGraph(i));
+        copies[node] = new Node(node->val);
+        queue<Node*> q;
+        q.push(node);
+        while(!q.empty()){
+            Node* curr = q.front();
+            q.pop();
+            for(auto i:curr->neighbors){
+                if(!copies.count(i)){
+                    copies[i] = new Node(i->val);
+                    q.push(i);
+                }
+                copies[curr]->neighbors.push_back(copies[i]);
             }
         }
         return copies[node];
