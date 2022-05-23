7c193ffa3509c68e86c74f38b4597fb942d83a0c
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-23 16:48:31 +0530

    Time: 49 ms (73.90%), Space: 17.5 MB (86.61%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -4,9 +4,9 @@ public:
     
     void dfs(vector<vector<int>>& heights, vector<vector<bool>> &visited, int i, int j){
         int m = heights.size(), n = heights[0].size();
-        if(i<0 || j<0 || i==m || j==n) {
-            return;
-        }
+        // if(i<0 || j<0 || i==m || j==n) {
+        //     return;
+        // }
         
         if(visited[i][j] == true)
             return;
@@ -14,12 +14,14 @@ public:
         visited[i][j] = true;
         
         // do dfs
-        for(auto dir:direction){
-            int next_i = i+dir[0];
-            int next_j = j+dir[1];
-            if(next_i>=0 && next_i<m && next_j>=0 && next_j<n && heights[next_i][next_j] >= heights[i][j])
-                dfs(heights, visited, next_i, next_j);
-        }
+        if(i+1<m && heights[i+1][j]>=heights[i][j])
+            dfs(heights,visited,i+1,j);
+        if(i-1>=0 && heights[i-1][j]>=heights[i][j])
+            dfs(heights,visited,i-1,j);
+        if(j+1<n && heights[i][j+1]>=heights[i][j])
+            dfs(heights,visited,i,j+1);
+        if(j-1>=0 && heights[i][j-1]>=heights[i][j])
+            dfs(heights,visited,i,j-1);
     }
     
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
