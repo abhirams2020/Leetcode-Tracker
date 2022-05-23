6663a33c5bbfee860703f8456b766b122502dddb
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-23 13:57:25 +0530

    Time: 7 ms (72.84%), Space: 8.9 MB (49.50%) - LeetHub

--- 133-clone-graph/133-clone-graph.cpp
+++ 133-clone-graph/133-clone-graph.cpp
@@ -21,18 +21,21 @@ public:
 
 class Solution {
 public:
+    // map to store new copy of an existing node
+    // copies[old node of 1] returns new node of 1 if it exist
     unordered_map<Node*,Node*> copies;
-        
+
     Node* cloneGraph(Node* node) {
         if(node==NULL){
             return NULL;
         }
-        if(copies.count(node)){
-            return copies[node];
-        }
-        copies[node] = new Node(node->val);
-        for(auto i:node->neighbors){
-            copies[node]->neighbors.push_back(cloneGraph(i));
+        // if copy already has node, then we dont need to find its neighbours
+        // if not, add all neighbours of old node to new node
+        if(!copies.count(node)){
+            copies[node] = new Node(node->val);
+            for(auto i:node->neighbors){
+                copies[node]->neighbors.push_back(cloneGraph(i));
+            }
         }
         return copies[node];
     }
