00e47e86ca68774055de6db7e39ec3ae24b8d847
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-23 22:30:40 +0530

    Time: 23 ms (27.97%), Space: 10.2 MB (35.68%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.cpp
+++ 130-surrounded-regions/130-surrounded-regions.cpp
@@ -2,8 +2,8 @@ class Solution {
 private:
     int m = 0, n = 0;
     vector<vector<bool>> visited;
-public:
     
+public:
     void dfs(vector<vector<char>>& board, int i, int j){
         if(i<0 || j<0 || i>=m || j>=n)
             return;
@@ -20,12 +20,14 @@ public:
         dfs(board,i,j-1);
     }
     
+    // Convert an 'O' to 'X' if it cannot reach edge of matrix
+    // We do inward dfs, ie starting from an 'O' on the edge and visiting the nearby 'O'
+    // Visited matrix stores the position of 'O' which can reach the edges
+    // Stop the dfs if already visited or it is an 'X'
     void solve(vector<vector<char>>& board) {
         m = board.size();
         n = board[0].size();
-        
         visited = vector<vector<bool>> (m, vector<bool> (n,false));
-        
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(i==0 || j==0 || i==m-1 || j==n-1){
@@ -33,7 +35,6 @@ public:
                 }
             }
         }
-        
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(visited[i][j] == false){
@@ -41,7 +42,5 @@ public:
                 }
             }
         }
-        
-        return;
     }
 };
