b122e9e2140a1e13d95ab1907c258daf0683ead4
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-25 18:52:02 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (61.57%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
-
+    vector<string> mapping = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    vector<string> res;
     
-    vector<string> ans;
-    vector<string> mp = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-    void solve(string& digits, int index, string& op){
-        if(index==digits.size()){
-            if(op!="")
-                ans.push_back(op);
+    void dfs(string digits, string op, int index){
+        if(index==digits.length()){
+            if(op.length()!=0)
+                res.push_back(op);
             return;
         }
-        for(int i=0;i<mp[(digits[index])-'0'].length();i++){
-            op.push_back(mp[(digits[index])-'0'][i]);
-            solve(digits,index+1,op);
+        int curr = digits[index] - '0';
+        for(auto c:mapping[curr]){
+            op.push_back(c);
+            dfs(digits,op,index+1);
             op.pop_back();
         }
     }
     
     vector<string> letterCombinations(string digits) {
-        unordered_map<char,string> mp;
-        mp['2'] = "abc";
-        mp['3'] = "def";
-        mp['4'] = "ghi";
-        mp['5'] = "jkl";
-        mp['6'] = "mno";
-        mp['7'] = "pqrs";
-        mp['8'] = "tuv";
-        mp['9'] = "wxyz";
-        string op = "";
-        solve(digits,0,op);
-        return ans;
+        string op="";
+        dfs(digits,op,0);
+        return res;
     }
 };
