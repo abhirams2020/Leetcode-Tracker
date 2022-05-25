44ea2c66bf86ab1542604f742b275764bc8a8ac5
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-26 01:22:48 +0530

    Time: 3 ms (32.88%), Space: 6.2 MB (30.53%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,24 +1,41 @@
 class Solution {
 public:
-    vector<int> dp;
     
-    int solve(int n){
-        if(n==0){
-            return 1;
-        }
-        if(n<0){
-            return 0;
-        }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        return dp[n] = solve(n-1) + solve(n-2);
-    }
+//     // RECURSIVE DP WITH MEMOIZATION
+//     vector<int> dp;
     
+//     int solve(int n){
+//         if(n==0){
+//             return 1;
+//         }
+//         if(n<0){
+//             return 0;
+//         }
+//         if(dp[n]!=-1){
+//             return dp[n];
+//         }
+//         return dp[n] = solve(n-1) + solve(n-2);
+//     }
+    
+//     int climbStairs(int n) {
+//         // dp = vector<int> (n+1, -1);
+//         dp.resize(n+1,-1);
+//         int res =  solve(n);
+//         return res;
+//     }
+    
+    // ITERATIVE DP
     int climbStairs(int n) {
-        // dp = vector<int> (n+1, -1);
-        dp.resize(n+1,-1);
-        int res =  solve(n);
-        return res;
+        // vector dp shows how many ways to reach 0 at ith position
+        // if we are in 0, 1 way to reach 0. ie no jump
+        // if we are on 1, 1 way to reach 0, ie 1 jump
+        vector<int> dp(n+1);
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i=2;i<n+1;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
