d57bdd03f8591c6c5628d463d1c648e8c649feda
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-26 01:16:12 +0530

    Time: 0 ms (100.00%), Space: 6 MB (39.26%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        vector<int> count(n+1,0);
-        count[0] = 1;
-        count[1] = 1;
-        for(int i=2;i<=n;i++){
-            count[i] = count[i-1] + count[i-2];
+    vector<int> dp;
+    
+    int solve(int n){
+        if(n==0){
+            return 1;
         }
-        return count[n];
+        if(n<0){
+            return 0;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        return dp[n] = solve(n-1) + solve(n-2);
     }
-//     int dp[46];
-//     int solve(int n){
-//         if(n<0){
-//             return 0;
-//         }
-//         if(n==0){
-//             return 1;
-//         }
-//         if(dp[n]!=-1){
-//             return dp[n];
-//         }
-//         return dp[n]=solve(n-1) + solve(n-2);        
-//     }
-
-//     int climbStairs(int n) {
-//         memset(dp,-1,sizeof(dp));
-//         return solve(n);
-//     }
+    
+    int climbStairs(int n) {
+        dp.resize(n+1,-1);
+        int res =  solve(n);
+        for(auto i:dp){
+            cout<<i<<" ";
+        }
+        return res;
+    }
 };
