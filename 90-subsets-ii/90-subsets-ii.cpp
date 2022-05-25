88b4003a5869be2fb1a2c96b30027139de4536d4
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-25 16:07:46 +0530

    Time: 7 ms (44.48%), Space: 8.1 MB (43.57%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -6,6 +6,8 @@ public:
     void solve(vector<int> &nums, vector<int> op, int index){
         res.push_back(op);
         for(int i=index;i<nums.size();i++){
+            // if next element is equal to curr element there will be repetition
+            // ie. {1,2,2}. current op is {1}. if we add 2 repeatedly, op will be {1,2} and {1,2}
             if(i==index || nums[i]!=nums[i-1]){
                 op.push_back(nums[i]);
                 solve(nums,op,i+1);
