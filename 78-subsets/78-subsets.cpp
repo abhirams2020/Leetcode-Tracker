b8507ce372a1080edceee280c4fb815b85f8c269
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-25 15:25:15 +0530

    Time: 8 ms (18.63%), Space: 16.1 MB (14.62%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,19 +1,31 @@
 class Solution {
+private:
+    vector<vector<int>> res;
+    
 public:
-    void solve(vector<int>& nums, vector<vector<int>>& res, vector<int> op, int index){
-        res.push_back(op);
-        for(int i=index;i<nums.size();i++){
-            op.push_back(nums[i]);
-            solve(nums, res, op, i+1);
-            op.pop_back();
+    void solve(vector<int> &nums, vector<int> op, int index){
+        if(index==nums.size()){
+            res.push_back(op);
+            return;
         }
-
+        solve(nums,op,index+1);
+        op.push_back(nums[index]);
+        solve(nums,op,index+1);
+        op.pop_back();
     }
     
+    // void solve(vector<int>& nums, vector<int> op, int index){
+    //     res.push_back(op);
+    //     for(int i=index;i<nums.size();i++){
+    //         op.push_back(nums[i]);
+    //         solve(nums, op, i+1);
+    //         op.pop_back();
+    //     }
+    // }
+    
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> res;
         vector<int> op;
-        solve(nums, res, op, 0);
+        solve(nums, op, 0);
         return res;
     }
 };
