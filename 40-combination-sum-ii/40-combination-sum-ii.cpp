158300faa4279ac5f39230a0fa7508e964c55a42
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-25 16:15:05 +0530

    Time: 29 ms (15.69%), Space: 12.4 MB (24.91%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -1,39 +1,28 @@
 class Solution {
 public:
-    
     vector<vector<int>> res;
     
-    void solve(vector<int> &candidates, int target, int index, vector<int>& op){
-
+    void solve(vector<int> &nums, vector<int> op, int target, int index){
         if(target == 0){
             res.push_back(op);
             return;
         }
-        if(index==candidates.size()){
-            return;
-        }        
-        if(candidates[index]>target){
+        if(index==nums.size() || target < nums[index]){
             return;
         }
-        for(int i=index;i<candidates.size();i++){
-            if(candidates[i]>target){
-                return;
+        for(int i=index;i<nums.size();i++){
+            if(i==index || nums[i]!=nums[i-1]){
+                op.push_back(nums[i]);
+                solve(nums,op,target-nums[i],i+1);
+                op.pop_back();
             }
-            if(i>index && candidates[i]==candidates[i-1]){
-                continue;
-            }
-            op.push_back(candidates[i]);
-            solve(candidates, target-candidates[i], i+1, op);
-            op.pop_back();
         }
-
     }
     
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
         vector<int> op;
-        sort(candidates.begin(),candidates.end());
-        solve(candidates,target,0,op);
+        solve(candidates, op, target, 0);
         return res;
     }
-    
 };
