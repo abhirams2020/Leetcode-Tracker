692d0a179cb2dcb80864567efd2be3b138870c05
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-25 18:14:16 +0530

    Time: 434 ms (58.31%), Space: 8.1 MB (36.79%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,27 +1,42 @@
 class Solution {
 public:
+    int m=0, n=0;
     
-    bool dfs(vector<vector<char>>& board, string &word, int i, int j, int index){
-        if(index==word.size()){
+    vector<vector<bool>> visited;
+    
+    bool dfs(vector<vector<char>> &board, string &word, int index, int i, int j){
+        if(index == word.size()){
             return true;
         }
-        if (i<0 || i>=board.size() || j<0 || j>=board[0].size() || board[i][j] != word[index]){  
+        if(i<0 || j<0 || i>=m || j>=n){
             return false;
         }
-        char temp = board[i][j];
-        board[i][j] = '#';
-        bool ans = dfs(board,word,i-1,j,index+1) || dfs(board,word,i+1,j,index+1) || dfs(board,word,i,j+1,index+1) || dfs(board,word,i,j-1,index+1);
-        board[i][j] = temp;
+        if(visited[i][j]==true || word[index]!=board[i][j]){
+            return false;
+        }
+        visited[i][j] = true;
+        bool ans =  dfs(board,word,index+1,i-1,j) ||
+                    dfs(board,word,index+1,i+1,j) ||
+                    dfs(board,word,index+1,i,j-1) ||
+                    dfs(board,word,index+1,i,j+1);
+        visited[i][j] = false;
         return ans;
     }
+    
     bool exist(vector<vector<char>>& board, string word) {
-         for(int i=0;i<board.size();i++){
-            for(int j=0;j<board[0].size();j++){       
-                if (dfs(board,word,i,j,0)){
-                    return true;
+        m = board.size();
+        n = board[0].size();
+        
+        visited = vector<vector<bool>> (m, vector<bool>(n));
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j] == word[0] && dfs(board,word,0,i,j)){
+                        return true;
                 }
             }
-         }
+        }
+        
         return false;
     }
 };
