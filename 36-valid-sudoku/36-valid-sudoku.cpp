da122135c575e6196e87c20b376ddb2b9dbbe8fc
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-26 13:25:25 +0530

    Time: 79 ms (5.19%), Space: 22 MB (5.84%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -1,84 +1,91 @@
 class Solution {
 public:
-    int dpRow[9];
-    int dpCol[9];
-    int dpGrid[3][3];
-    bool check(vector<vector<char>>& board, int row, int col){
-
-        if(row==board.size()-1 && col==board.size()-1){
+    vector<int> dpRow;
+    vector<int> dpCol;
+    vector<vector<int>> dpGrid;
+    
+    bool isValid(vector<vector<char>>& board, int row, int col){
+        if(row==9){
             return true;
         }
-        if(col==board.size()){
-            return check(board, row+1, 0);
+        if(col==9){
+            return isValid(board, row+1, 0);
         }
-        if(board[row][col]=='.'){
-            return check(board, row, col+1);
-        }
-        unordered_set<char> s;
-        if(dpCol[col]!=-1){
-            if(dpCol[col]==0){
-                return false;
-            }
-        }
-        else {
-            for(int i=0;i<9;i++){
-                if(s.count(board[i][col])){
-                    dpCol[col]=0;
-                    return false;
-                }
-                if(board[i][col]!='.')
-                    s.insert(board[i][col]);
-            }
-            dpCol[col]=1;
-            s.clear();            
+        if(board[row][col]!='.'){
+            return isValid(board,row,col+1);
         }
         
+        // CHECK DP COL
+        if(dpCol[col]!=-1){
+            if(dpCol[col]==0)
+                return dpCol[col];
+        }
+        else {
+            // CHECK COLUMN
+            unordered_set<char> colSet;
+            for(int i=0;i<9;i++){
+                if(colSet.count(board[i][col])){
+                    return dpCol[col] = 0;
+                }
+                if(board[i][col]!='.')
+                    colSet.insert(board[i][col]);
+            }
+            dpCol[col] = 1;
+        }
+        
+        // CHECK DP ROW
         if(dpRow[row]!=-1){
-            if(dpRow[row]==0){
+            if(dpRow[row]==0)
+                return dpRow[row];
+        }
+        else {
+            // CHECK ROW
+            unordered_set<char> rowSet;
+            for(int j=0;j<9;j++){
+                if(rowSet.count(board[row][j])){
+                    return dpRow[row] = 0;
+                }
+                if(board[row][j]!='.')
+                    rowSet.insert(board[row][j]);
+            }
+            dpRow[row] = 1;    
+        }
+        
+        // CHECK DP GRID
+
+        int rowIndex = 3 * (row/3);
+        int colIndex = 3 * (col/3);
+        int gridRow = row/3;
+        int gridCol = col/3;
+        
+        if(dpGrid[gridRow][gridCol]!=-1){
+            if(dpGrid[gridRow][gridCol]==0){
                 return false;
             }
         }
         else {
-            for(int j=0;j<9;j++){
-                if(s.count(board[row][j])){
-                    dpRow[row] = 0;
-                    return false;
-                }
-                if(board[row][j]!='.')
-                    s.insert(board[row][j]);
-            }
-            dpRow[row] = 1;
-            s.clear();            
-        }
-
-        int dpRow = row/3, dpCol = col/3;
-        if(dpGrid[dpRow][dpCol]!=-1){
-            if(dpGrid[dpRow][dpCol]==0){
-                return false;
-            }
-        }
-        else{        
-            int gridRow = 3*(row/3);
-            int gridCol = 3*(col/3);
-            for(int i=gridRow; i<gridRow+3;i++){
-                for(int j=gridCol; j<gridCol+3; j++){
-                    if(s.count(board[i][j])){
-                        dpGrid[dpRow][dpCol] = 0;
-                        return false;
+            // CHECK GRID
+            unordered_set<char> gridSet;
+            for(int i=rowIndex;i<rowIndex+3;i++){
+                for(int j=colIndex;j<colIndex+3;j++){
+                    if(gridSet.count(board[i][j])){
+                        return dpGrid[gridRow][gridCol] = 0;
                     }
                     if(board[i][j]!='.')
-                        s.insert(board[i][j]);
+                        gridSet.insert(board[i][j]);
                 }
             }
-            dpGrid[dpRow][dpCol] = 1;
+            dpGrid[gridRow][gridCol] = 1;
         }
-        return check(board, row, col+1);
+        
+        // CURR POS IS VALID, SO CHECK NEXT POS 
+        return isValid(board,row,col+1);
     }
     
     bool isValidSudoku(vector<vector<char>>& board) {
-        memset(dpCol,-1,sizeof(dpCol));
-        memset(dpRow,-1,sizeof(dpRow));
-        memset(dpGrid,-1,sizeof(dpGrid));
-        return check(board,0,0);
+        dpRow.resize(9,-1);
+        dpCol.resize(9,-1);
+        dpGrid.resize(3, vector<int>(3,-1));
+        return isValid(board,0,0);
     }
 };
