a5ef8ee606a3d1fd2e4f947395a140d20adc7697
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-27 01:48:51 +0530

    Time: 8 ms (68.83%), Space: 6.8 MB (63.21%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.cpp
+++ 43-multiply-strings/43-multiply-strings.cpp
@@ -5,11 +5,12 @@ public:
             return "0";
         }
         
+        // Max possible size of product will be m+n
         int m=num1.length(), n=num2.length();
         vector<int> arr(m+n,0);
         
-        int carry = 0;
-        
+        // Do reverse traversal and update the digit places
+        // Carry of i'th position is added to i-1'th position
         for(int i=m-1;i>=0;i--){
             for(int j=n-1;j>=0;j--){
                 int currPos = i+j+1, prevPos = i+j;
@@ -23,6 +24,7 @@ public:
         string res;
         
         for(int i=0;i<m+n;i++){
+            // Skip leading zeroes
             if(res.empty() && arr[i]==0){
                 continue;
             }
