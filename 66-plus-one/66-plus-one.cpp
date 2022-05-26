b520c34b9cffdc4a6f418702536c5c049ae5970d
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-26 19:02:21 +0530

    Time: 3 ms (60.58%), Space: 8.9 MB (12.89%) - LeetHub

--- 66-plus-one/66-plus-one.cpp
+++ 66-plus-one/66-plus-one.cpp
@@ -5,9 +5,12 @@ public:
         int n = digits.size();
         
         for(int i=n-1;i>=0;i--){
-            digits[i] = (digits[i]+1)%10;
-            if(digits[i]!=0){
+            if(digits[i]<9){
+                digits[i]++;
                 return digits;
+            }
+            else {
+                digits[i]=0;
             }
         }
         digits[0] = 1;
