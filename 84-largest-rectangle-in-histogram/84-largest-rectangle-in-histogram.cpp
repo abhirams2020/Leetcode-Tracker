350a3039dd07e38b40307c1fc5606d9ffa03ec2c
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-26 16:31:02 +0530

    Time: 228 ms (55.77%), Space: 80.6 MB (48.43%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,43 +1,29 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        stack<int> s;
         int n = heights.size();
         vector<int> left(n);
         vector<int> right(n);
         
+        left[0] = -1;
+        right[n-1] = n;
+        
         // FINDING NEAREST SMALLER TO LEFT
-        for(int i=0;i<n;i++){
-            while(!s.empty() && heights[s.top()]>=heights[i]){
-                s.pop();
+        for(int i=1;i<n;i++){
+            int prev = i-1;
+            while(prev>=0 && heights[prev]>=heights[i]){
+                prev = left[prev];
             }
-            if(s.empty()){
-                left[i] = -1;
-            }
-            else {
-                left[i] = s.top();
-            }
-            s.push(i);
+            left[i] = prev;
         }
         
-        // CLEAR THE STACK
-        // while(!s.empty()){
-        //     s.pop();
-        // }
-        s = {};
-        
         // FINDING NEAREST SMALLER TO RIGHT
-        for(int i=n-1;i>=0;i--){
-            while(!s.empty() && heights[s.top()]>=heights[i]){
-                s.pop();
+        for(int i=n-2;i>=0;i--){
+            int prev = i+1;
+            while(prev<n && heights[prev]>=heights[i]){
+                prev = right[prev];
             }
-            if(s.empty()){
-                right[i] = n;
-            }
-            else {
-                right[i] = s.top();
-            }
-            s.push(i);
+            right[i] = prev;
         }
         
         // CALCULATE MAX AREA
@@ -46,8 +32,8 @@ public:
         // RIGHT CONTAINS POSITION OF NEAREST SMALLER ELEMENT TO RIGHT
         
         // AREA = heights[i] * (DISTANCE BETWEEN RIGHT AND LEFT)
-        // DISTANCE = R-L+1. R is right[i]-1. L is left[i]+1
-        // DISTANCE = right[i]-1-left[i]-1+1 = right[i]-left[i]-1
+        // WIDTH = R-L+1. R is right[i]-1. L is left[i]+1
+        // WIDTH = right[i]-1-left[i]-1+1 = right[i]-left[i]-1
         int maxArea = 0;
         for(int i=0;i<n;i++){
             int currArea = heights[i] * (right[i] - left[i] - 1);
