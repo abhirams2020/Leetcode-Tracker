279e5c7fc49a1bac6d447c5bb542a8f86420d348
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-26 19:04:06 +0530

    Time: 4 ms (40.26%), Space: 8.8 MB (12.89%) - LeetHub

--- 66-plus-one/66-plus-one.cpp
+++ 66-plus-one/66-plus-one.cpp
@@ -5,6 +5,8 @@ public:
         int n = digits.size();
         
         for(int i=n-1;i>=0;i--){
+            // KEEP MAKING 9 TO 0 TILL WE GET NO LESS THAN 9
+            // INCREMENT THAT NO BY 1 AND RETURN THE VECTOR
             if(digits[i]<9){
                 digits[i]++;
                 return digits;
@@ -13,8 +15,13 @@ public:
                 digits[i]=0;
             }
         }
+        
+        // IF ALL DIGITS ARE 9, WE NEED TO ADD 1 IN FRONT
+        
+        // digits.push(digits.begin(),1);
         digits[0] = 1;
         digits.push_back(0);
+        
         return digits;
     }
 };
