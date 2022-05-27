9247b0de94a1835b6cdac800e096b3e98a553818
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-27 23:44:05 +0530

    Time: 4 ms (35.90%), Space: 8 MB (17.52%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
+    vector<vector<int>> dp;
     
-    int dp[2][100];
-    
-    int solve(vector<int> &nums, int i, int index){
-        if(index>=nums.size()){
+    int solve(vector<int> &nums, int dpIndex, int index, int end){
+        if(index > end){
             return 0;
         }
-        if(dp[i][index]!=-1){
-            cout<<i<<" "<<index<<" "<<dp[i][index]<<endl;
-            return dp[i][index];
+        if(dp[dpIndex][index] != -1){
+            return dp[dpIndex][index];
         }
-        // include nums[index] and move to index+2 OR not include nums[index] and move to index+1
-        return dp[i][index] = max(solve(nums,i,index+1) ,nums[index] + solve(nums,i,index+2));
+        return dp[dpIndex][index] = max(solve(nums,dpIndex,index+1,end), nums[index] + solve(nums,dpIndex,index+2,end));
     }
     
     int rob(vector<int>& nums) {
-        if(nums.size()==1){
+        int n = nums.size();
+        if(n==1){
             return nums[0];
         }
-        memset(dp,-1,sizeof(dp));
-        vector<int> v1(nums.begin() + 1, nums.end());
-        vector<int> v2(nums.begin(), nums.end() - 1);
-        return max(solve(v1,0,0),solve(v2,1,0));
+        dp.resize(2,vector<int>(n+1,-1));
+        int res = max(solve(nums,0,0,n-2), solve(nums,1,1,n-1));
+        return res;
     }
 };
