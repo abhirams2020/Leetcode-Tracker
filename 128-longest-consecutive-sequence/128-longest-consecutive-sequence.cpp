10a8e2197659cbb1b9bd9cdeec05318e720af130
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-28 20:20:54 +0530

    Time: 227 ms (46.13%), Space: 49.8 MB (13.55%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,85 +1,35 @@
 class Solution {
 public:
-    // int longestConsecutive(vector<int>& nums) {
-    //     set<int> s;
-    //     for(auto i:nums){
-    //         s.insert(i);
-    //     }
-    //     int count = 1;
-    //     int maxcount = 0;
-    //     int prev = INT_MIN/2;
-    //     for(auto curr:s){
-    //         if(curr-prev==1){
-    //             count++;
-    //         }
-    //         else {
-    //             count=1;
-    //         }
-    //         prev = curr;
-    //         maxcount = max(maxcount, count);
-    //     }
-    //     return maxcount;
-    // }
-
-    //     int longestConsecutive(vector<int>& nums) {
-    //     if(nums.size()==0){
-    //         return 0;
-    //     }
-    //     unordered_map<int,int> mp;
-    //     int maxVal = 1, n;
-    //     for(int i=0;i<nums.size();i++){
-    //         mp[nums[i]] = i;
-    //     }
-    //     for(int i=0;i<nums.size();i++){
-    //         if(nums[i]!=INT_MAX){
-    //             int count = 1;
-    //             n = nums[i]+1;
-    //             while(mp.count(n)){
-    //                 count++;
-    //                 nums[mp[n]] = INT_MAX;
-    //                 mp.erase(n);
-    //                 n++;
-    //             }
-    //             n = nums[i]-1;
-    //             while(mp.count(n)){
-    //                 count++;
-    //                 nums[mp[n]] = INT_MAX;
-    //                 mp.erase(n);
-    //                 n--;
-    //             }
-    //             maxVal = max(maxVal, count);
-    //         }
-    //     }
-    //     return maxVal;
-    // }
-    
     int longestConsecutive(vector<int>& nums) {
         if(nums.size()==0){
             return 0;
         }
+        
+        int maxSize = 1;
+        
         unordered_set<int> s;
         for(auto i:nums){
             s.insert(i);
         }
-        int maxcount = 1;
+        
         while(!s.empty()){
-            int val = *s.begin();
-            s.erase(val);
-            int count = 1;
-            
-            // val, val+1, val+2 ...
-            for(int i=val+1;s.count(i);i++){
-                s.erase(i);
-                count++;
+            int curr = *s.begin();
+            int size = 1;
+            int num = curr;
+            s.erase(curr);
+            while(s.count(num-1)){
+                size++;
+                s.erase(num-1);
+                num--;
             }
-            
-            // val-2, val-1, val ...
-            for(int i=val-1;s.count(i);i--){
-                s.erase(i);
-                count++;
+            num = curr;
+            while(s.count(num+1)){
+                size++;
+                s.erase(num+1);
+                num++;
             }
-            maxcount = max(maxcount,count);
+            maxSize = max(maxSize,size);
         }
-        return maxcount;
+        return maxSize;
     }
 };
