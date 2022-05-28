19a739e9d897f4421eedc597da59196ce4f8b0c9
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-29 00:04:01 +0530

    Time: 21 ms (35.74%), Space: 8.1 MB (24.58%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    
     bool checkInclusion(string s1, string s2) {
-        unordered_map<char, int> mp1, mp2;
+        unordered_map<char,int> mp1,mp2;
         for(auto i:s1){
             mp1[i]++;
         }
-        int l=0, r=0;
-        for(r=0; r<s2.length(); r++) {
+        int l=0;
+        for(int r=0;r<s2.length();r++){
             mp2[s2[r]]++;
-            if(r-l+1 == s1.length()) {
-                if(mp1 == mp2){
+            
+            // compare mp1 and mp2 when window size same as s1.length()
+            if(r-l+1 == s1.length()){
+                if(mp1==mp2){
                     return true;
                 }
-                
-                // move left ptr
                 mp2[s2[l]]--;
-                if(mp2[s2[l]] == 0){
+                if(mp2[s2[l]]==0){
                     mp2.erase(s2[l]);
                 }
                 l++;
