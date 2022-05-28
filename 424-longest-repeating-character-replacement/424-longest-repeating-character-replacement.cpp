30416471af40fe3c5ee8af984c914bc88285a4bc
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-28 23:51:43 +0530

    Time: 72 ms (9.83%), Space: 7.1 MB (55.14%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -9,6 +9,11 @@ public:
         for(int r=0;r<s.length();r++){
             mp[s[r]]++;
             
+            // if count of s[r] >= count of maxChar, update maxChar
+            
+            // eg. s=BAAA,k=0. 
+            // if i=1 count of A,B are same.If not updated, mp['B'] becomes 0 but (r-l+1)-mp[maxChar] = 1.
+            // So it will remove 'A' also. If we updated maxChar to 'A' while adding 'A', this would not happen.
             if(mp[s[r]]>=mp[maxChar]){
                 maxChar = s[r];
             }
@@ -16,6 +21,7 @@ public:
             // we don't need loop to update maxChar since maxcount in the window is count[maxChar].
             // when we move l to right, the maximum of counts remains less than the maxcount.
             while((r-l+1)-mp[maxChar] > k){
+                cout<<s[l]<<endl;
                 mp[s[l]]--;
                 l++;
             }
