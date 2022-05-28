c370077193b2f101f87483fff0d0ff773eabc633
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-28 17:47:45 +0530

    Time: 111 ms (70.68%), Space: 51.4 MB (51.72%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -2,33 +2,13 @@ class Solution {
 public:
     // CHECK IF ELEMENT ALREADY EXIST BEFORE ADDING TO SET
     
-    // bool containsDuplicate(vector<int>& nums) {
-    //     unordered_set<int> s;
-    //     for(auto i:nums){
-    //         if(s.count(i)!=0){
-    //             return true;
-    //         }
-    //         s.insert(i);
-    //     }
-    //     return false;
-    // }
-    
-    // SORT ARRAY AND CHECK IF ADJACENT ELEMENTS ARE SAME
-    
-    // bool containsDuplicate(vector<int> &nums){
-    //     sort(nums.begin(),nums.end());
-    //     for(int i=1;i<nums.size();i++){
-    //         if(nums[i-1]==nums[i]){
-    //             return true;
-    //         }
-    //     }
-    //     return false;
-    // }
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]==nums[i+1])
+        unordered_set<int> s;
+        for(auto i:nums){
+            if(s.count(i)!=0){
                 return true;
+            }
+            s.insert(i);
         }
         return false;
     }
