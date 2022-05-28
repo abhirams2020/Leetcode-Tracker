bcb1423b575c587e07db14f6ac8d29c923864551
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-28 17:46:50 +0530

    Time: 155 ms (24.87%), Space: 46.5 MB (97.57%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,12 +1,26 @@
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> s;
-        for(auto i:nums){
-            if(s.count(i)!=0){
+    // CHECK IF ELEMENT ALREADY EXIST BEFORE ADDING TO SET
+    
+    // bool containsDuplicate(vector<int>& nums) {
+    //     unordered_set<int> s;
+    //     for(auto i:nums){
+    //         if(s.count(i)!=0){
+    //             return true;
+    //         }
+    //         s.insert(i);
+    //     }
+    //     return false;
+    // }
+    
+    // SORT ARRAY AND CHECK IF ADJACENT ELEMENTS ARE SAME
+    
+    bool containsDuplicate(vector<int> &nums){
+        sort(nums.begin(),nums.end());
+        for(int i=1;i<nums.size();i++){
+            if(nums[i-1]==nums[i]){
                 return true;
             }
-            s.insert(i);
         }
         return false;
     }
