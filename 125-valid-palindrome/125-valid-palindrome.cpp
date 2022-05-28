9cea61c41b1ade5d63390470ec8a7ae679316fc3
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-28 21:23:04 +0530

    Time: 8 ms (51.19%), Space: 7.2 MB (89.66%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -4,12 +4,18 @@ public:
         int n = s.length();
         int left = 0, right = n-1;
         while(left < right){
+            
+            // move left ptr till we find alphanumeric element
             while(left<n && !isalnum(s[left])){
                 left++;
             }
+            
+            // move right ptr till we find alphanumeric elemtnt
             while(right>=0 && !isalnum(s[right])){
                 right--;
             }
+            
+            // if no alphanumeric element is there, left will be greater than right
             if(left<right && tolower(s[left])!=tolower(s[right])){
                 return false;
             }
