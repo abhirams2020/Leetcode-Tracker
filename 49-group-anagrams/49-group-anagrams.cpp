4a6baebba3a6887104b87e23033a72b883adf1dd
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-28 19:16:59 +0530

    Time: 84 ms (16.97%), Space: 20.5 MB (61.02%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> res;
         // map[string] will give all anagrams of the string
-        map<string, vector<string>> mp;
+        unordered_map<string, vector<string>> mp;
         
         for(int i=0;i<strs.size();i++){
             string tempstr = strs[i];
