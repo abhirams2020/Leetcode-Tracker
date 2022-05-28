c1c0809f13778d6372eaa0ee2d5c802e8d16557b
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-28 22:40:36 +0530

    Time: 27 ms (19.64%), Space: 19.7 MB (14.38%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,44 +1,24 @@
 class Solution {
 public:
-//     O(N) space
-//     int trap(vector<int>& height) {
-//         int left=0, right=0, sum=0, n = height.size();
-//         vector<int> maxLeft(n), maxRight(n);
-        
-//         for(int i=0;i<n;i++){
-//             left = max(height[i],left);
-//             maxLeft[i] = left;
-//         }
-        
-//         for(int i=n-1; i>=0;i--){
-//             right = max(height[i],right);
-//             maxRight[i] = right;
-//         }
-    
-//         for(int i=0;i<n;i++){
-//             sum += min(maxLeft[i],maxRight[i]) - height[i];
-//         }
-//         return sum;
-//     }
-    
-//  O(1) space two pointer approach
     int trap(vector<int>& height) {
-        int maxLeft=0, maxRight=0, sum=0;
         int left=0, right=height.size()-1;
+        int maxLeft=0;
+        int maxRight=0;
+        int water = 0;
         
         while(left < right){
             if(height[left] < height[right]){
-                maxLeft = max(maxLeft, height[left]);
-                sum += maxLeft - height[left];
+                maxLeft = max(maxLeft,height[left]);
+                water += maxLeft-height[left];
                 left++;
             }
             else {
-                maxRight = max(maxRight, height[right]);
-                sum += maxRight - height[right];
+                maxRight = max(maxRight,height[right]);
+                water += maxRight-height[right];
                 right--;
             }
         }
         
-        return sum;
+        return water;
     }
 };
