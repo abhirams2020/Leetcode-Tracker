e83bff8920a293fbc76ea7d9e96f173ff1d4644f
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-28 19:15:53 +0530

    Time: 57 ms (41.20%), Space: 21 MB (28.39%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,43 +1,20 @@
 class Solution {
 public:
-
-//     string getkey(string &str){
-//         string res;
-//         vector<int> count(26,0);
-//         for(auto i:str){
-//             count[i-'a']++;
-//         }
-//         for(int i=0;i<26;i++){
-//             for(int j=0;j<count[i];j++){
-//                 res.push_back(char('a'+i));
-//             }
-//         }
-//         return res;
-//     }
-
-//     vector<vector<string>> groupAnagrams(vector<string> &strs){
-//         vector<vector<string>> res;
-//         unordered_map<string,vector<string>> mp;
-//         for(int i=0;i<strs.size();i++){
-//             mp[getkey(strs[i])].push_back(strs[i]);
-//         }
-//         for(auto i:mp){
-//             res.push_back(i.second);
-//         }
-//         return res;
-//     }
-    
-    vector<vector<string>> groupAnagrams(vector<string> strs){
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> res;
-        unordered_map<string,vector<string>> mp;
+        // map[string] will give all anagrams of the string
+        map<string, vector<string>> mp;
+        
         for(int i=0;i<strs.size();i++){
-            string s = strs[i];
-            sort(s.begin(),s.end());
-            mp[s].push_back(strs[i]);
+            string tempstr = strs[i];
+            sort(tempstr.begin(), tempstr.end());
+            mp[tempstr].push_back(strs[i]);
         }
+        
         for(auto i:mp){
             res.push_back(i.second);
         }
+        
         return res;
     }
 };
