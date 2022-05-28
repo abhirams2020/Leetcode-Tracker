251cab25cd085fa466b14375daff901b9149fcee
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-28 19:35:27 +0530

    Time: 41 ms (26.79%), Space: 23.8 MB (99.35%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -1,18 +1,40 @@
 class Solution {
 public:
+    
+//     // O(2*N) space and O(N) time complexity
+//     vector<int> productExceptSelf(vector<int>& nums) {
+//         int n = nums.size();
+        
+//         // leftprod stores product of all numbers to left of i
+//         // rightprod stores product of all number to right of i
+//         vector<int> leftProd(n,1), rightProd(n,1);
+//         vector<int> ans(n);
+        
+//         for(int i=1;i<n;i++){
+//             leftProd[i] = nums[i-1]*leftProd[i-1];
+//         }
+//         for(int i=n-2;i>=0;i--){
+//             rightProd[i] = nums[i+1]*rightProd[i+1];
+//         }
+//         for(int i=0;i<n;i++){
+//             ans[i] = leftProd[i]*rightProd[i];
+//         }
+//         return ans;
+//     }
+    
+    // O(1) space and O(N) time complexity
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int> leftProd(n,1), rightProd(n,1);
-        vector<int> ans(n);
+        int leftProd = 1, rightProd = 1;
+        vector<int> ans(n,1);
         
-        for(int i=1;i<n;i++){
-            leftProd[i] = nums[i-1]*leftProd[i-1];
-        }
-        for(int i=n-2;i>=0;i--){
-            rightProd[i] = nums[i+1]*rightProd[i+1];
-        }
         for(int i=0;i<n;i++){
-            ans[i] = leftProd[i]*rightProd[i];
+            ans[i] = ans[i] * leftProd;
+            leftProd = leftProd * nums[i];
+        }
+        for(int i=n-1;i>=0;i--){
+            ans[i] = ans[i] * rightProd;
+            rightProd = rightProd * nums[i];
         }
         return ans;
     }
