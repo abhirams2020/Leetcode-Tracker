6c9dc87d3c2d58d21f4c9cd14ab0c0612a5c708b
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-29 18:18:35 +0530

    Time: 32 ms (65.10%), Space: 22.4 MB (9.60%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -1,43 +1,52 @@
 class Solution {
 public:
-    // USING XOR
-    int singleNonDuplicate(vector<int>& nums) {
-        int ans = 0;
-        for(auto i:nums){
-            ans = ans^i;
-        }
-        return ans;
-    }
-    
-    // // USING BINARY SEARCH
+    // // USING XOR
     // int singleNonDuplicate(vector<int>& nums) {
-    //     if(nums.size()==1){
-    //         return nums[0];
+    //     int ans = 0;
+    //     for(auto i:nums){
+    //         ans = ans^i;
     //     }
-    //     int left=0, right=nums.size()-1;
-    //     while(left<=right){
-    //         int mid = (left+right)/2;
-    //         cout<<mid<<endl;
-    //         if(mid>0 && nums[mid]==nums[mid-1]){
-    //             if((left-mid+1)%2==0){
-    //                 left = mid+1;
-    //             }
-    //             else {
-    //                 right = mid;
-    //             }
-    //         }
-    //         else if(mid<nums.size()-1 && nums[mid]==nums[mid+1]){
-    //             if((mid-right+1)%2==0){
-    //                 right = mid-1;
-    //             }
-    //             else {
-    //                 left = mid;
-    //             }                
-    //         }
-    //         else {
-    //             return nums[mid];
-    //         }
-    //     }
-    //     return 0;
+    //     return ans;
     // }
+    
+    // USING BINARY SEARCH
+    
+    // size of array with 1 number appearing once and others twice will be ODD
+    
+    // if nums[mid]==nums[mid-1], l to mid is one possibility, mid+1 to r is other possibility
+    // if l-mid+1 is even, move left to mid+1, else move right to mid
+    
+    // if nums[mid]==nums[mid+1], l to mid-1 is one possibility, mid to r is other possibility
+    // if mid-r+1 is even, move right to mid-1, else move left to mid
+    
+    int singleNonDuplicate(vector<int>& nums) {
+        if(nums.size()==1){
+            return nums[0];
+        }
+        int left=0, right=nums.size()-1;
+        while(left<=right){
+            int mid = (left+right)/2;
+            cout<<mid<<endl;
+            if(mid>0 && nums[mid]==nums[mid-1]){
+                if((left-mid+1)%2==0){
+                    left = mid+1;
+                }
+                else {
+                    right = mid;
+                }
+            }
+            else if(mid<nums.size()-1 && nums[mid]==nums[mid+1]){
+                if((mid-right+1)%2==0){
+                    right = mid-1;
+                }
+                else {
+                    left = mid;
+                }                
+            }
+            else {
+                return nums[mid];
+            }
+        }
+        return 0;
+    }
 };
