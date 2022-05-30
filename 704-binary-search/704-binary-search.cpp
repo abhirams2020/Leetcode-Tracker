253df23a0ca227acb0619d0f7f248a28ac8fdf25
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-30 14:29:02 +0530

    Time: 52 ms (45.47%), Space: 27.6 MB (64.22%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int l=0,r=nums.size()-1,mid;
-        while(l<=r){
-            mid = l+(r-l)/2;
-            if(nums[mid] == target)
+        int left=0,right=nums.size()-1,mid;
+        while(left<=right){
+            mid = left+(right-left)/2;
+            if(nums[mid]==target){
                 return mid;
+            }
             else if(nums[mid]<target){
-                l = mid+1;
+                left = mid+1;
             }
             else {
-                r = mid-1;
+                right = mid-1;
             }
         }
         return -1;
