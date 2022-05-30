bf6537e29808cfaeef9d73a5680c2f0304f556f2
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-30 19:14:53 +0530

    Time: 9 ms (24.11%), Space: 9.5 MB (79.36%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        // start from last col in row 0.
+        // at that pt, move left if target < matrix[row][col]
+        // move down if target > matrix[row][col]. if equal then return true
         int row = 0, col = matrix[0].size()-1;
         while(row<matrix.size() && col>=0){
             if(matrix[row][col]==target){
