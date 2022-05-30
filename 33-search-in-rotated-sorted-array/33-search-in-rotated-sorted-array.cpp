50184227b26eb25fc38fe38a81e4a455fa16c53b
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-30 20:56:15 +0530

    Time: 10 ms (19.56%), Space: 11 MB (75.75%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -9,19 +9,22 @@ public:
             }
             // if left side of mid is sorted
             else if(nums[left]<=nums[mid]){
-                // if target not in left side of mid
+                // if target is in left side of mid, search range left to mid
                 if(target >= nums[left] && target <= nums[mid]){
                     right = mid;
                 }
+                // if target not in left side, search mid+1 to right range
                 else {
                     left = mid + 1;
                 }
             }
             // if right side of mid is sorted
             else if(nums[mid]<=nums[right]){
+                // if target is in right side of mid, search range right to mid
                 if(target >= nums[mid] && target <= nums[right]){
                     left = mid;
                 }
+                // if target not in right side, search left to mid-1 range
                 else {
                     right = mid - 1;
                 }
