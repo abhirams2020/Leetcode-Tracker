9c801572da25005c90a7fd5425c3778dafbeff62
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-31 19:39:26 +0530

    Time: 63 ms (24.17%), Space: 71.4 MB (50.48%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -14,29 +14,30 @@ public:
         ListNode* dummy = new ListNode();
         ListNode* curr = dummy;
         int carry = 0;
+        
+        // add sum of both to new nodes
         while(l1 && l2){
             int sum = l1->val + l2->val + carry;
-
+            // if sum >= 10, enter only unit digit in new list
             ListNode* newnode = new ListNode(sum%10);
             curr->next = newnode;
             curr = curr->next;
             l1 = l1->next;
             l2 = l2->next;
-            
+            // update new carry
             if(sum>=10)
                 carry = 1;
             else 
                 carry = 0;
-            
         }
-
+        
+        // if l1 or l2 still left, add those to new list
         while(l1){
             int sum = l1->val + carry;
             ListNode* newnode = new ListNode(sum%10);
             curr->next = newnode;
             curr = curr->next;
             l1 = l1->next;
-            
             if(sum>=10)
                 carry = 1;
             else 
@@ -49,13 +50,13 @@ public:
             curr->next = newnode;
             curr = curr->next;
             l2 = l2->next;
-            
             if(sum>=10)
                 carry = 1;
             else 
                 carry = 0;
         }
         
+        // if still carry remains, add 1 to end of list. eg 9999 + 999 = 10998
         if(carry == 1){
             ListNode* newnode = new ListNode(1);
             curr->next = newnode;
