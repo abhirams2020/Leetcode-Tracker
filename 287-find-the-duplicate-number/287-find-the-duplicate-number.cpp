f4b5704455e24edb9ed7790c2752161ffa8fd75a
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-31 16:30:40 +0530

    Time: 170 ms (49.27%), Space: 61.3 MB (61.38%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
+    // there are n+1 numbers in range [1,n].
+    // when a number is found. make nums[number] to negative
+    // negative at index i indicates i was found before
     int findDuplicate(vector<int>& nums) {
         for(int i=0;i<nums.size();i++){
             if(nums[abs(nums[i])] < 0){
                 return abs(nums[i]);
             }
-            nums[abs(nums[i])] = -abs(nums[abs(nums[i])]);
+            nums[abs(nums[i])] = -(nums[abs(nums[i])]);
         }
         return -1;
     }
