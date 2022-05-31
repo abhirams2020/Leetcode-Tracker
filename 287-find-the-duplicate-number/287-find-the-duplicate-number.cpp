382c5db14bbab148a4942fae43a80ed3a7116327
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-05-31 16:47:52 +0530

    Time: 120 ms (86.38%), Space: 61.3 MB (27.75%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,15 +1,37 @@
 class Solution {
 public:
-    // there are n+1 numbers in range [1,n].
-    // when a number is found. make nums[abs(number)] to negative
-    // negative at index i indicates i was found before
+    // // there are n+1 numbers in range [1,n].
+    // // when a number is found. make nums[abs(number)] to negative
+    // // negative at index i indicates i was found before
+    // int findDuplicate(vector<int>& nums) {
+    //     for(int i=0;i<nums.size();i++){
+    //         if(nums[abs(nums[i])] < 0){
+    //             return abs(nums[i]);
+    //         }
+    //         nums[abs(nums[i])] = -(nums[abs(nums[i])]);
+    //     }
+    //     return -1;
+    // }
+    
+    // using linked list cycle concept
     int findDuplicate(vector<int>& nums) {
-        for(int i=0;i<nums.size();i++){
-            if(nums[abs(nums[i])] < 0){
-                return abs(nums[i]);
+        int fast = nums[0], slow = nums[0];
+        
+        // loop till we find the cycle
+        while(true){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if(fast == slow){
+                break;
             }
-            nums[abs(nums[i])] = -(nums[abs(nums[i])]);
         }
-        return -1;
+        
+        // find start of cycle by pointing slow to start of list
+        slow = nums[0];
+        while(fast!=slow){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
     }
 };
