8201881db34f750ec2d7bb8a607d08da8b2d5aa2
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-02 00:25:26 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (24.50%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
+    // dp stores no of ways from start pointer
     vector<int> dp;
     
     bool isValid(string &s, int start, int end){
         string str = s.substr(start,end-start+1);
         // valid is 1,2,3...9, 10,11,...,19, 20,21,...,26
+        // number starting with 0 not valid
         if(str[0]=='0'){
             return false;
         }
+        // if 2 digit number start with 3 or more, invalid
+        // if 2 digit number start with 2 and more than 6, invalid.
         if(str.length()==2 && (str[0]>='3' || (str[0]=='2' && str[1]>'6'))){
             return false;
         }
@@ -15,6 +19,7 @@ public:
     }
     
     int solve(string &s, int start, int end){
+        // if start more than end, means all substrings were decoded
         if(start>end){
             return 1;
         }
@@ -22,6 +27,7 @@ public:
             return dp[start];
         }
         int count = 0;
+        // count possibilities if we take 1 char from start or 2 chars from start
         for(int i=start;i<=end && i<=start+1;i++){
             if(isValid(s,start,i)){
                 count += solve(s,i+1,end);
