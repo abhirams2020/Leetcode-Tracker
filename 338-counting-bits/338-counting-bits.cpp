24ef6350d5360a4112fb9d65a902166268ad45f0
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-01 14:23:20 +0530

    Time: 13 ms (19.01%), Space: 8.6 MB (21.30%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,19 +1,10 @@
 class Solution {
 public:
-    
-    unordered_map<int,int> dp;
-    
     int count1s(int n){
         if(n==0){
             return 0;
         }
-        if(n==1){
-            return 1;
-        }
-        if(dp.find(n)!=dp.end()){
-            return dp[n];
-        }
-        return dp[n] = count1s(n>>1) + (n&1);
+        return (n&1) + count1s(n>>1);
     }
     
     vector<int> countBits(int n) {
@@ -21,7 +12,6 @@ public:
         for(int i=0;i<=n;i++){
             res.push_back(count1s(i));
         }
-        cout<<count1s(5);
         return res;
     }
 };
