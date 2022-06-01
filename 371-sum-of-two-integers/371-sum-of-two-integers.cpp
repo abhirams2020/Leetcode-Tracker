6784d42b95223da6336ac15b66c181b32c9ba281
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-01 18:33:52 +0530

    Time: 4 ms (19.93%), Space: 5.8 MB (73.50%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -2,9 +2,14 @@ class Solution {
 public:
     int getSum(int a, int b) {
         // store sum in a and carry in b
+        // sum of 2 digits can be found by using XOR
+        // ie if 0,0 or 1,1 that place sum will be 0. else 1
+        // carry is found by doing AND. carry only if both are 1
+        // unsigned carry to avoid overflow when negative shift to left
+        
         while(b!=0){
             int sum = a^b;
-            int carry = (a&b);
+            int carry = a&b;
             a = sum;
             b = ((unsigned int)carry)<<1;
         }
