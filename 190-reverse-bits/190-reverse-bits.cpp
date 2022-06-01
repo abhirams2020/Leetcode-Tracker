0a32aa2ce634eade4ab0f9e0bce53fef06534d9f
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-01 16:33:34 +0530

    Time: 3 ms (63.07%), Space: 6 MB (51.24%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.cpp
+++ 190-reverse-bits/190-reverse-bits.cpp
@@ -2,9 +2,14 @@ class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
         uint32_t ans = 0;
+        // since 32 digits are there, we repeat 32 times
+        // each time add unit digit to ans and move it left by 1 place
+        // move n to right by 1 place
+        // instead of adding to ans, we can also use OR for speed.
+        // we do ans<<1 first since at last digit we should not do ans<<1
         for(int i=0;i<32;i++){
             ans = ans<<1;
-            ans = ans | (n&1);
+            ans = ans | (n&1); // ans + (n&1)
             n = n>>1;
         }
         return ans;
