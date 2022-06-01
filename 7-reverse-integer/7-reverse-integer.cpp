89e3bd690f8ca5128883abab6c392dd5765fa216
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-01 19:49:38 +0530

    Time: 0 ms (100.00%), Space: 6 MB (33.46%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.cpp
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -2,15 +2,21 @@ class Solution {
 public:
     int reverse(int x) {
         int ans = 0;
+        // new ans = ans*10 + digit
+        // if new ans overflows INT_MAX, it means ans*10 + digit > INT_MAX
+        // there are two possibilities, ans*10 > INT_MAX or ans=INT_MAX/10
+        // INT_MAX/10 = 2147483647/10 = 214748364.
+        // So for valid ans, last digit should be <= 7.
         while(x){
             int digit = x%10;
-            cout<<ans<<" "<<digit<<endl;
+            
             if(ans > INT_MAX/10 || (ans==INT_MAX/10 && digit >= INT_MAX%10)){
                 return 0;
             }
             if(ans < INT_MIN/10 || (ans==INT_MAX/10 && digit <= INT_MIN%10)){
                 return 0;
             }
+            
             ans = ans*10 + digit;
             x = x/10;
         }
