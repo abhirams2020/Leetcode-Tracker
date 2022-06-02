b43342e79b2737fc137623f554cdcd399f74a553
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-02 17:26:20 +0530

    Time: 481 ms (43.29%), Space: 128 MB (91.65%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.cpp
+++ 981-time-based-key-value-store/981-time-based-key-value-store.cpp
@@ -1,42 +1,81 @@
-class TimeMap {
-public:
-    // since time is increasing order, store time and value in vector
-    unordered_map<string, vector<pair<int,string>>> mp; // key, {timestamp, value}
+// class TimeMap {
+// public:
+//     // since time is increasing order, store time and value in vector
+//     unordered_map<string, vector<pair<int,string>>> mp; // key, {timestamp, value}
     
+//     TimeMap() {
+        
+//     }
+    
+//     void set(string key, string value, int timestamp) {
+//         mp[key].push_back({timestamp,value});
+//     }
+    
+//     // search highest value less than or equal to timestamp
+//     string searchVal(vector<pair<int,string>> &arr, int timestamp){
+//         int l = 0, r = arr.size()-1;
+//         int last = -1; // storing last occurrance of valid value
+//         while(l<=r){
+//             int mid = (l+r)/2;
+//             if(arr[mid].first <= timestamp){
+//                 last = mid;
+//                 l = mid+1;
+//             }
+//             else {
+//                 r = mid-1;
+//             }
+//         }
+
+//         if(last==-1){
+//             return ""; // no valid value found
+//         }
+//         return arr[last].second;
+//     }
+    
+//     string get(string key, int timestamp) {
+//         if(mp.find(key)==mp.end()){
+//             return "";
+//         }
+//         return searchVal(mp[key],timestamp);
+//     }
+// };
+
+class TimeMap {
+private:
+    unordered_map<string, vector<pair<string, int>>> map; // key, {value, timestamp}
+    
+    string searchVal(vector<pair<string, int>> &vec, const int &timestamp) {
+        int low = 0;
+        int high = vec.size() - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if (vec[mid].second > timestamp) {
+                high = mid - 1;
+            } else if (vec[mid].second < timestamp) {
+                low = mid + 1;
+            } else {
+                return vec[mid].first;
+            }
+        }
+        
+        return high >= 0 ? vec[high].first : "";
+    }
+public:
     TimeMap() {
         
     }
     
     void set(string key, string value, int timestamp) {
-        mp[key].push_back({timestamp,value});
-    }
-    
-    // search highest value less than or equal to timestamp
-    string searchVal(vector<pair<int,string>> &arr, int &timestamp){
-        int l = 0, r = arr.size()-1;
-        int last = -1; // storing last occurrance of valid value
-        while(l<=r){
-            int mid = (l+r)/2;
-            if(arr[mid].first <= timestamp){
-                last = mid;
-                l = mid+1;
-            }
-            else {
-                r = mid-1;
-            }
-        }
-
-        if(last==-1){
-            return ""; // no valid value found
-        }
-        return arr[last].second;
+        map[key].push_back({value, timestamp});
     }
     
     string get(string key, int timestamp) {
-        if(mp.find(key)==mp.end()){
+        if (map.find(key) == map.end()) {
             return "";
         }
-        return searchVal(mp[key],timestamp);
+        return searchVal(map[key], timestamp);
     }
 };
 
