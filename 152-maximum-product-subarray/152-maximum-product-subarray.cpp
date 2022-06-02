889248e391f43e278a371297077fc23f824f5b14
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-03 02:27:52 +0530

    Time: 7 ms (82.79%), Space: 13.7 MB (73.42%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        if(nums.size()==0) 
-            return 0;
-        
         int currMin=nums[0], currMax=nums[0], maxProd=nums[0];
-        
         for(int i=1;i<nums.size();i++){
-            // prevMax and prevMin temporarily stores currMax and currMin before modification
-            // if we don't use prevMax and prevMin, modified currMax will be used to calculate currMin
-            
-            int prevMax = currMax, prevMin = currMin;
-            currMax = max(max(prevMin*nums[i],prevMax*nums[i]), nums[i]);
-            currMin = min(min(prevMax*nums[i],prevMin*nums[i]), nums[i]);
+            int prevMin = currMin, prevMax = currMax;
+            currMin = min(nums[i], min(prevMin*nums[i], prevMax*nums[i]));
+            currMax = max(nums[i], max(prevMin*nums[i], prevMax*nums[i]));
             maxProd = max(maxProd, currMax);
         }
         return maxProd;
