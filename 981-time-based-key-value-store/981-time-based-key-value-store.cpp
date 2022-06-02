660c6af0e51a165e0af440886c425aa5cbb80d22
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-02 17:19:32 +0530

    Time: 690 ms (7.30%), Space: 128.1 MB (82.34%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.cpp
+++ 981-time-based-key-value-store/981-time-based-key-value-store.cpp
@@ -1,23 +1,41 @@
 class TimeMap {
 public:
-    map<pair<int,string>, string> mp;
+    unordered_map<string, vector<pair<int,string>>> mp;
     
     TimeMap() {
         
     }
     
     void set(string key, string value, int timestamp) {
-        mp[{timestamp,key}] = value;
-        // cout<<mp[{timestamp,key}]<<endl;
+        mp[key].push_back({timestamp,value});
+    }
+    
+    // search highest value less than or equal to timestamp
+    string searchVal(vector<pair<int,string>> &arr, int timestamp){
+        int l = 0, r = arr.size()-1;
+        int last = -1; // storing last occurrance of valid value
+        while(l<=r){
+            int mid = (l+r)/2;
+            if(arr[mid].first <= timestamp){
+                last = mid;
+                l = mid+1;
+            }
+            else {
+                r = mid-1;
+            }
+        }
+
+        if(last==-1){
+            return ""; // no valid value found
+        }
+        return arr[last].second;
     }
     
     string get(string key, int timestamp) {
-        for(int i=timestamp;i>=1;i--){
-            if(mp.find({i,key})!=mp.end()){
-                return mp[{i,key}];
-            }
+        if(mp.count(key)==0){
+            return "";
         }
-        return "";
+        return searchVal(mp[key],timestamp);
     }
 };
 
