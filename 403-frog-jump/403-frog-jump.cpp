3eb062da495a298b88fd7e248accf4e37ffc8c85
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-03 08:48:04 +0530

    Time: 98 ms (89.33%), Space: 235.3 MB (9.93%) - LeetHub

---
+++ 403-frog-jump/403-frog-jump.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    unordered_map<int,int> mp;
+    
+    bool solve(vector<int> &stones, int dist, int jump){
+        if(mp.count(dist)==0 || jump==0){
+            return false;
+        }
+        int index = mp[dist];
+        if(index == stones.size()-1){
+            return true;
+        }
+        if(dp[index][jump]!=-1){
+            return dp[index][jump];
+        }
+        dist = dist+jump;
+        return dp[index][jump] = (solve(stones,dist,jump-1) || solve(stones,dist,jump) || solve(stones,dist,jump+1));
+    }
+    
+    bool canCross(vector<int>& stones) {
+        dp.resize(1+stones.size(), vector<int> (1+stones.size(), -1));
+        // initial jump should be from 0 to 1.
+        int jump = 1;
+        for(int i=0;i<stones.size();i++){
+            mp[stones[i]] = i;
+        }
+        return solve(stones, 0, jump);
+    }
+};
