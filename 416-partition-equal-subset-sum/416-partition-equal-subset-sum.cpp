72148c5dd054b3381c8b54ac10ef673e300fff05
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-03 09:34:53 +0530

    Time: 409 ms (24.44%), Space: 128.9 MB (8.94%) - LeetHub

---
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    
+    bool targetSum(vector<int> &nums, int index, int sum) {
+        if(sum == 0){
+            return true;
+        }
+        if(index == nums.size()){
+            return false;
+        }
+        if(dp[index][sum]!=-1){
+            return dp[index][sum];
+        }
+        bool include = false, notInclude = false;
+        if(nums[index] <= sum){
+            include = targetSum(nums,index+1,sum-nums[index]);
+        }
+        notInclude = targetSum(nums,index+1,sum);
+        return dp[index][sum] = (include || notInclude);
+    }
+    
+    bool canPartition(vector<int>& nums) {
+        int sum = 0;
+        for(auto i:nums){
+            sum += i;
+        }
+        // if sum is odd, it cannot be partitioned into 2
+        if(sum%2 == 1){
+            return false;
+        }
+        // we have to find subsequence with sum = sum/2
+        dp.resize(201, vector<int> (1+(sum/2), -1));
+        return targetSum(nums, 0, sum/2);
+    }
+};
