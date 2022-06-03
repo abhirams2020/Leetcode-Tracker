e9d014768213fe6c785f716d39334cbc2d3b72e6
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-03 08:54:12 +0530

    Time: 85 ms (93.21%), Space: 234.8 MB (11.22%) - LeetHub

--- 403-frog-jump/403-frog-jump.cpp
+++ 403-frog-jump/403-frog-jump.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
+    // dp[i][j] stores result for j jump from an index i
     vector<vector<int>> dp;
+    // map to check if curr dist is valid index and get index from dist
     unordered_map<int,int> mp;
     
     bool solve(vector<int> &stones, int dist, int jump){
+        // if curr pos is not valid or jump is 0
+        dist = dist+jump;
         if(mp.count(dist)==0 || jump==0){
             return false;
         }
         int index = mp[dist];
+        // if index reaches last pos
         if(index == stones.size()-1){
             return true;
         }
         if(dp[index][jump]!=-1){
             return dp[index][jump];
         }
-        dist = dist+jump;
         return dp[index][jump] = (solve(stones,dist,jump-1) || solve(stones,dist,jump) || solve(stones,dist,jump+1));
     }
     
     bool canCross(vector<int>& stones) {
+        // max jump possible is size of array
         dp.resize(stones.size(), vector<int> (stones.size(), -1));
         // initial jump should be from 0 to 1.
         int jump = 1;
