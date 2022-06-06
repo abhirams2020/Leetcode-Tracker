fcf98e448dbd982ddba4c03d319f3cfadeaf1a24
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-06 14:18:53 +0530

    Time: 312 ms (45.73%), Space: 78.7 MB (58.54%) - LeetHub

--- 853-car-fleet/853-car-fleet.cpp
+++ 853-car-fleet/853-car-fleet.cpp
@@ -1,41 +1,19 @@
 class Solution {
 public:
-    // Sort cars by descending order of position
-    // if a car behind curr car reaches before curr car, means they meet at some point
-    // when cars meet, take min of the speeds. so max of time taken is considered
-    // if stack empty or stack.top() > curr_time. i.e. ahead car time taken to reach target is more,
-    // then they meet once and can be considered as a fleet. 
-    // In this case, we take min speed or the max time taken to reach target which is stack.top() itself
-    // If stack.top() < curr_time, ie ahead car is faster, then push curr_time to stack
-    // For every car, if it meets the car ahead it should get the speed of the ahead car
-    // So, if a car is following another car and will not meet, we add the following car speed.
-    // The speed of the following car is made the speed of cars behind it if they meet.
-    
-    // eg.  If A,B,C are  following each other and time taken to reach target in the order C,A,B
-    //      then the order of reaching target will be 1st..A  2nd..B&C. Since B is ahead of C, when both meet
-    //      speed of B is taken by C and it reaches the target along with B.
-    
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
-        vector<pair<int,int>> nums;
-        stack<double> time;
-        int n = position.size();
-        for(int i=0;i<n;i++){
-            nums.push_back({position[i],speed[i]});
+        priority_queue<pair<int,float>> pq;
+        for(int i=0;i<position.size();i++){
+            float time = (target-position[i])/(float)speed[i];
+            pq.push({position[i], time});
         }
-    
-        sort(nums.begin(),nums.end(),greater<>());
-        
-        for(int i=0;i<n;i++){
-            double timeRem = ((double)target-nums[i].first)/nums[i].second;
-            if(time.empty()){
-                time.push(timeRem);
+        int count = 0;
+        while(!pq.empty()){
+            float currTime = pq.top().second;
+            while(!pq.empty() && pq.top().second<=currTime){
+                pq.pop();
             }
-            else {
-                if(time.top() < timeRem){
-                    time.push(timeRem);
-                }
-            }
+            count++;
         }
-        return time.size();
+        return count;
     }
 };
