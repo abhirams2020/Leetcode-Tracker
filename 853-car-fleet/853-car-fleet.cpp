ebec0aaaf44a5336fd88a26d87273c4477e036dd
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-06 14:24:38 +0530

    Time: 314 ms (44.82%), Space: 86 MB (35.50%) - LeetHub

--- 853-car-fleet/853-car-fleet.cpp
+++ 853-car-fleet/853-car-fleet.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
+    // Sort cars by descending order of position
+    // if a car behind curr car reaches before curr car, means they meet at some point
+    // when cars meet, take min of the speeds. so max of time taken is considered
+    // For every car, if it meets the car ahead it should get the speed of the ahead car
+    
+    // if time taken by car behind curr is less than time taken by curr, they will arrive as a fleet
+    // remove all pos behind curr which has time <= curr time. All these will be single fleet. Then increment count.
+    // repeat till the priority queue is empty
+    
+    // eg.  If A,B,C are  following each other and time taken to reach target in the order C,A,B
+    //      then the order of reaching target will be 1st..A  2nd..B&C. Since B is ahead of C, when both meet
+    //      speed of B is taken by C and it reaches the target along with B.
+    
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        // priority queue to store {position, time}
         priority_queue<pair<int,double>> pq;
         for(int i=0;i<position.size();i++){
             double time = (target-position[i])/(double)speed[i];
