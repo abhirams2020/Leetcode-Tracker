77c3d5b8396c13e80870fd0df1ac736a35c2633c
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-10 03:27:07 +0530

    Time: 1350 ms (66.58%), Space: 558.2 MB (80.13%) - LeetHub

--- 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.cpp
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.cpp
@@ -1,3 +1,62 @@
+// // USING DICTIONARY TO STORE NEXT CHARACTERS AND ITS NODE
+// class TrieNode {
+// public:
+//     bool is_word;
+//     // map stores next node of current characters
+//     unordered_map<int,TrieNode*> children;
+//     TrieNode() {
+//         is_word = false;
+//     }
+// };
+
+// class WordDictionary {
+// private:
+//     TrieNode* root;
+    
+// public:
+//     /** Initialize your data structure here. */
+//     WordDictionary() {
+//         root = new TrieNode();
+//     }
+    
+//     /** Adds a word into the data structure. */
+//     void addWord(string word) {
+//         TrieNode* curr = root;
+//         for(auto c:word){
+//             if(curr->children.count(c-'a') == 0){
+//                 curr->children[c-'a'] = new TrieNode();
+//             }
+//             curr = curr->children[c-'a'];
+//         }
+//         curr->is_word = true;
+//     }
+    
+//     /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+//     bool search(string word) {
+//         return searchTrie(word,0,root);
+//     }
+    
+//     // function to check if word from index i is present in trie starting at curr
+//     bool searchTrie(string &word, int index, TrieNode* curr) {
+//         for (int i = index; i<word.length() ; i++) {
+//             if (word[i] == '.'){
+//                 for (auto node:curr->children) {
+//                     if (searchTrie(word, i+1, node.second)) {
+//                         return true;
+//                     }
+//                 }
+//                 return false;
+//             }
+//             if(curr->children.count(word[i]-'a') == 0){
+//                 return false;
+//             }
+//             curr = curr->children[word[i]-'a'];
+//         }
+//         return curr->is_word;
+//     }
+// };
+
+// USING VECTOR TO STORE NEXT NODES
 class TrieNode {
 public:
     bool is_word;
@@ -57,6 +116,7 @@ public:
         return curr->is_word;
     }
 };
+
 
 /**
  * Your WordDictionary object will be instantiated and called as such:
