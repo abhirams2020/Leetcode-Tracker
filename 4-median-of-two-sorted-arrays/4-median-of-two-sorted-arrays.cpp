6c7851d7e02376a894683b77867131053637db50
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-11 18:43:34 +0530

    Time: 62 ms (38.13%), Space: 89.4 MB (47.30%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int mid = (nums1.size()+nums2.size())/ 2;
-        double prev_m=0, m=0;
-        int f1=0,f2=0;
-        while(mid+1>0 && f1<nums1.size() && f2<nums2.size()){
-            if(nums1[f1] < nums2[f2]){
-                prev_m = m;
-                m = nums1[f1];
-                f1++;
+        int n1 = nums1.size(), n2 = nums2.size();
+        if(n1 > n2){
+            return findMedianSortedArrays(nums2,nums1);
+        }
+        // no of elements in left partition of the merged array
+        int half = (n1 + n2)/2;
+        int left = -1 , right = n1;
+        while(left<=right){
+            int m1 = floor((left+right)/2.0);
+            int m2 = half - m1 - 2;
+            
+            int l1 = (m1<0)?INT_MIN:nums1[m1];
+            int l2 = (m2<0)?INT_MIN:nums2[m2];
+            int r1 = (m1+1>=n1)?INT_MAX:nums1[m1+1];
+            int r2 = (m2+1>=n2)?INT_MAX:nums2[m2+1];
+            // cout<<m1<<" "<<m2<<endl;
+            // cout<<l1<<" "<<r2<<" : "<<l2<<" "<<r1<<endl;
+            if(l1<=r2 && l2<=r1){
+                if((n1+n2)%2 == 0){
+                    return (max(l1,l2)+min(r1,r2))/2.0;
+                }
+                return min(r1,r2);
             }
-            else {
-                prev_m = m;
-                m = nums2[f2]; 
-                f2++;
+            else if(l1>r2){
+                right = m1-1;
             }
-            mid--;
+            else if(l2>r1){
+                left = m1+1;
+            }
         }
-        while(mid+1>0 && f1<nums1.size()){
-            prev_m = m;
-            m = nums1[f1];
-            f1++;
-            mid--;
-        }
-        while(mid+1>0 && f2<nums2.size()){
-            prev_m = m;
-            m = nums2[f2]; 
-            f2++;
-            mid--;
-        }
-        if((nums1.size()+nums2.size())% 2 == 0){
-            return (prev_m + m)/2;
-        }
-        return m;
+        return 0;
     }
 };
