f1b326ff37609b303522dd4b0128506c1db29f97
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-13 12:27:33 +0530

    Time: 18 ms (74.60%), Space: 17.1 MB (39.12%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -41,22 +41,29 @@ public:
     
    // sort the intervals after inserting new interval and merge them
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<int> prev;
+        vector<int> prev(2);
         vector<vector<int>> res;
         int n = intervals.size();
         bool found = false;
         int i=0;
+        // add all intervals to res which end before start of newInterval
         while(i<n && intervals[i][1]<newInterval[0]){
             res.push_back(intervals[i]);
             i++;
         }
+        
+        // current interval overlaps which new interval, so change curr interval
         prev = newInterval;
+        
+        // merge intervals till end of prev is less than start of curr
         while(i<n && prev[1]>=intervals[i][0]){
             prev[0] = min(intervals[i][0], prev[0]);
             prev[1] = max(intervals[i][1], prev[1]);
             i++;
         }
+        // push the resulting prev after merging
         res.push_back(prev);
+        // add remaining intervals to result
         while(i<n){
             res.push_back(intervals[i]);
             i++;
