3864a76b63f2534bf11932f2cd37e3d9d7c3cac2
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-14 00:36:37 +0530

    Time: 602 ms (51.17%), Space: 89.8 MB (82.68%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
@@ -1,3 +1,5 @@
+// // SORTING BY END INTERVAL
+
 // class Solution {
 // public:
 //     static bool comp(vector<int>& a, vector<int>& b){
@@ -29,27 +31,25 @@
 class Solution 
 {
 public:
-    int eraseOverlapIntervals(vector<vector<int>>&v) 
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) 
     {
-        sort(v.begin(),v.end());
-        int count=0,l=0,r=1,n=v.size();
-        while(r<n)
-        {
-            if(v[l][1]<=v[r][0]) // Non-overlapping case(Case 1)
-            {
-                l=r;
-                r++;
+        int n = intervals.size();
+        int count = 0;
+        sort(intervals.begin(),intervals.end());
+        vector<int> prev = {INT_MIN,INT_MIN};
+        
+        for(int i=0;i<n;i++){
+            if(prev[1] <= intervals[i][0]){
+                prev = intervals[i];
             }
-            else if(v[l][1]<=v[r][1])  // Case 2
-            {
+            else {
+                if(prev[1]<=intervals[i][1]){
+                    // do nothing
+                }
+                else {
+                    prev = intervals[i];
+                }
                 count++;
-                r++;
-            }
-            else if(v[l][1]>v[r][1])   // Case 3
-            {
-                count++;
-                l=r;
-                r++;
             }
         }
         return count;
