c0c1e44b9666bc1fae4fc4146cb276dcf58481ea
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-14 00:27:08 +0530

    Time: 936 ms (9.37%), Space: 89.7 MB (82.68%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
@@ -1,27 +1,49 @@
 class Solution {
 public:
-    static bool comp(vector<int>& a, vector<int>& b){
+//     static bool comp(vector<int>& a, vector<int>& b){
+//         return a[1]<b[1];
+//     }
+    
+//     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+//         int n = intervals.size();
+//         int count = 0;
+        
+//         // sort elements wrt end index to remove min no of intervals
+//         // to remove max no of intervals, sort by start index
+//         sort(intervals.begin(),intervals.end(),comp);
+//         vector<int> prev = {INT_MIN,INT_MIN};
+        
+//         for(int i=0;i<n;i++){
+//             if(prev[1]>intervals[i][0]){
+//                 count++;
+//             }
+//             else {
+//                 prev = intervals[i];
+//             }
+//         }
+        
+//         return count;
+//     }
+    
+    static bool comp(vector<int>& a, vector<int>& b)
+    {
+        if(a[1] == b[1]) return a[0]<b[0];
         return a[1]<b[1];
     }
-    
+
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(),comp);
         int n = intervals.size();
-        int count = 0;
+        int ans = 0;
+        vector<int> p = intervals[0];
         
-        // sort elements wrt end index to remove min no of intervals
-        // to remove max no of intervals, sort by start index
-        sort(intervals.begin(),intervals.end(),comp);
-        vector<int> prev = {INT_MIN,INT_MIN};
-        
-        for(int i=0;i<n;i++){
-            if(prev[1]>intervals[i][0]){
-                count++;
-            }
-            else {
-                prev = intervals[i];
-            }
+        for(int i=1;i<n;i++)
+        {
+            if(p[1] > intervals[i][0])
+                ans++;
+            else
+                p = intervals[i];
         }
-        
-        return count;
+        return ans;
     }
 };
