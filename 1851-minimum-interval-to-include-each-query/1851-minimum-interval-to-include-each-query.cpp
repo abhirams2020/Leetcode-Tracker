033c33f31c934d2286c4d8d20c1583ce24ec7124
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-15 01:24:43 +0530

    Time: 706 ms (73.98%), Space: 121.8 MB (66.14%) - LeetHub

--- 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.cpp
+++ 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.cpp
@@ -6,29 +6,36 @@ public:
         vector<int> q = queries;
         sort(intervals.begin(),intervals.end());
         sort(q.begin(),q.end());
+        // unordered map to store min size for a given num
         unordered_map<int,int> mp;
         priority_queue<pii, vector<pii>, greater<pii>> pq;
+        
         int i = 0;
         for(auto num:q){
+            // keep only elements valid for curr num in the pq
+            // valid means end should be greater than or equal to num
+            // if intervals from previous num is valid, keep that (IMP)
             while(!pq.empty() && pq.top().second < num){
                 pq.pop();
             }
             
-            while(i < intervals.size()){
+            while(i<intervals.size()){
                 int start = intervals[i][0], end = intervals[i][1];
+                // if start > num, means all intervals from now is invalid. so break
                 if(start>num){
                     break;
                 }
-                if(end>=num){
+                // if end < num, means num not in this interval
+                if(num<=end){
                     pq.push({end-start+1, end});
                 }
                 i++;
             }
-            
-            
+            // pq is empty means no valid intervals found
             if(pq.empty()){
                 mp[num] = -1;
             }
+            // pq.top() stores valid interval with least size
             else {
                 mp[num] = pq.top().first;
             }
