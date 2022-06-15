0cbb8b38767dfcef9a25812b655a04b991c95272
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-15 17:53:31 +0530

    Time: 267 ms (64.78%), Space: 85.8 MB (39.41%) - LeetHub

--- 853-car-fleet/853-car-fleet.cpp
+++ 853-car-fleet/853-car-fleet.cpp
@@ -14,18 +14,40 @@ public:
     //      then the order of reaching target will be 1st..A  2nd..B&C. Since B is ahead of C, when both meet
     //      speed of B is taken by C and it reaches the target along with B.
     
+    // int carFleet(int target, vector<int>& position, vector<int>& speed) {
+    //     // priority queue to store {position, time}
+    //     priority_queue<pair<int,double>> pq;
+    //     for(int i=0;i<position.size();i++){
+    //         double time = (target-position[i])/(double)speed[i];
+    //         pq.push({position[i], time});
+    //     }
+    //     int count = 0;
+    //     while(!pq.empty()){
+    //         double currTime = pq.top().second;
+    //         while(!pq.empty() && pq.top().second<=currTime){
+    //             pq.pop();
+    //         }
+    //         count++;
+    //     }
+    //     return count;
+    // }
+    static bool comp(pair<int,double> &a, pair<int,double> &b) {
+        return a.first < b.first;
+    }
+    
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
-        // priority queue to store {position, time}
-        priority_queue<pair<int,double>> pq;
-        for(int i=0;i<position.size();i++){
-            double time = (target-position[i])/(double)speed[i];
-            pq.push({position[i], time});
-        }
+        vector<pair<int,double>> posTime;
         int count = 0;
-        while(!pq.empty()){
-            double currTime = pq.top().second;
-            while(!pq.empty() && pq.top().second<=currTime){
-                pq.pop();
+        for(int i=0;i<position.size();i++){
+            double time = (double)(target - position[i])/speed[i];
+            posTime.push_back({position[i],time});            
+        }
+        sort(posTime.begin(),posTime.end(),comp);
+        int i=position.size()-1;
+        while(i>=0){
+            double currTime = posTime[i].second;
+            while(i>=0 && posTime[i].second <= currTime){
+                i--;
             }
             count++;
         }
