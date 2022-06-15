eb377b3101816afb04dbbc8e3b85a523f30336b9
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-15 13:18:04 +0530

    Time: 11 ms (9.65%), Space: 12.2 MB (17.74%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
@@ -11,33 +11,72 @@
  */
 class Solution {
 public:
+    // // REVERSING THE LEVEL AFTER GETTING ALL ELEMENTS
+    // vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+    //     vector<vector<int>> res;
+    //     if(!root){
+    //         return res;
+    //     }
+    //     queue<TreeNode*> q;
+    //     q.push(root);
+    //     bool leftToRight = false;
+    //     while(!q.empty()){
+    //         int n = q.size();
+    //         vector<int> temp(n);
+    //         for(int i=0;i<n;i++){
+    //             TreeNode *curr = q.front();
+    //             if(leftToRight == true){
+    //                 temp[n-1-i] = curr->val;
+    //             }
+    //             else {
+    //                 temp[i] = curr->val;
+    //             }
+    //             if(curr->left)
+    //                 q.push(curr->left);
+    //             if(curr->right)
+    //                 q.push(curr->right);
+    //             q.pop();
+    //         }
+    //         res.push_back(temp);
+    //         leftToRight = !leftToRight;
+    //     }
+    //     return res;
+    // }
+    
+    // REVERSING ORDER OF ADDING ELEMENTS IN QUEUE
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         vector<vector<int>> res;
         if(!root){
             return res;
         }
-        queue<TreeNode*> q;
-        q.push(root);
-        bool flag = false;
+        deque<TreeNode*> q;
+        q.push_back(root);
+        bool leftToRight = false;
         while(!q.empty()){
             int n = q.size();
-            vector<int> temp(n);
+            vector<int> temp;
             for(int i=0;i<n;i++){
-                TreeNode *curr = q.front();
-                if(flag == true){
-                    temp[n-1-i] = curr->val;
+                if(leftToRight == false){
+                    TreeNode *curr = q.front();
+                    temp.push_back(curr->val);
+                    if(curr->left)
+                        q.push_back(curr->left);
+                    if(curr->right)
+                        q.push_back(curr->right);
+                    q.pop_front();
                 }
                 else {
-                    temp[i] = curr->val;
+                    TreeNode *curr = q.back();
+                    temp.push_back(curr->val);
+                    if(curr->right)
+                        q.push_front(curr->right);
+                    if(curr->left)
+                        q.push_front(curr->left);
+                    q.pop_back();
                 }
-                if(curr->left)
-                    q.push(curr->left);
-                if(curr->right)
-                    q.push(curr->right);
-                q.pop();
             }
             res.push_back(temp);
-            flag = !flag;
+            leftToRight = !leftToRight;
         }
         return res;
     }
