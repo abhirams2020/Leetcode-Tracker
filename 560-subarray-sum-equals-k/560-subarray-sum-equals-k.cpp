b200ca6ce623576a1fac20f3cd937e0c8f88b74e
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-15 16:08:20 +0530

    Time: 99 ms (74.38%), Space: 41.5 MB (52.35%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int count=0, sum=0;
-        // mp[sum] gives no of time that sum occurred before i
+        int count=0, prefixSum=0;
+        // mp[prefixSum] gives no of time that sum occurred before i
         unordered_map<int,int> mp;
+        // initialise mp[0]=1 to count elements with same value as k
         mp[0] = 1;
         for(int i=0;i<nums.size();i++){
-            sum += nums[i];
-            if(mp.count(sum-k)){
-                count += mp[sum-k];
+            prefixSum += nums[i];
+            if(mp.count(prefixSum-k)){
+                // if subarray with sum=k, found add number of such  subarrays to count
+                count += mp[prefixSum-k];
             }
-            mp[sum]++;
+            mp[prefixSum]++;
         }
         return count;
     }
