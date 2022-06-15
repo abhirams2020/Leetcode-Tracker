3329e8cfc85cd544d71177db78313bd59c6c3ac5
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-15 08:13:44 +0530

    Time: 13 ms (18.53%), Space: 7.4 MB (75.10%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -5,22 +5,37 @@ public:
         int left = 0, right = n-1;
         while(left < right){
             
-            // move left ptr till we find alphanumeric element
-            while(left<n && !isalnum(s[left])){
-                left++;
-            }
+//             // move left ptr till we find alphanumeric element
+//             while(left<n && !isalnum(s[left])){
+//                 left++;
+//             }
             
-            // move right ptr till we find alphanumeric elemtnt
-            while(right>=0 && !isalnum(s[right])){
+//             // move right ptr till we find alphanumeric elemtnt
+//             while(right>=0 && !isalnum(s[right])){
+//                 right--;
+//             }
+            
+//             // if no alphanumeric element is there, left will be greater than right
+//             if(left<right && tolower(s[left])!=tolower(s[right])){
+//                 return false;
+//             }
+//             left++;
+//             right--;
+            if(isalnum(s[left]) && isalnum(s[right])){
+                if(tolower(s[left])!=tolower(s[right])){
+                    return false;
+                }
+                left++;
                 right--;
             }
-            
-            // if no alphanumeric element is there, left will be greater than right
-            if(left<right && tolower(s[left])!=tolower(s[right])){
-                return false;
+            else {
+                if(!isalnum(s[left])){
+                    left++;
+                }
+                if(!isalnum(s[right])){
+                    right--;
+                }
             }
-            left++;
-            right--;
         }
         return true;
     }
