8195bc3140745248091c080a6bb1789a648397f3
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-17 00:27:31 +0530

    Time: 59 ms (10.04%), Space: 16.8 MB (8.00%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    unordered_map<int,vector<int>> mp;
+    unordered_set<int> visited;
+    unordered_set<int> noCycle;
+    
+    bool isCycle(int curr){
+        if(noCycle.count(curr)){
+            return false;
+        }
+        if(visited.count(curr)){
+            return true;
+        }
+        visited.insert(curr);
+        for(auto i:mp[curr]){
+            if(isCycle(i)){
+                return true;
+            }
+            else {
+                noCycle.insert(i);
+            }
+        }
+        visited.erase(curr);
+        return false;
+    }
+    
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        for(auto i:prerequisites){
+            mp[i[1]].push_back(i[0]);
+        }
+        for(auto i:prerequisites){
+            int curr = i[0];
+            if(isCycle(curr)){
+                return false;
+            }
+            noCycle.insert(curr);
+        }
+        return true;
+    }
+};
