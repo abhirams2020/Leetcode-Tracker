96f9600aa0195b6a0699740b8c02b0af9e3889b9
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-18 00:54:53 +0530

    Time: 26 ms (56.52%), Space: 9.1 MB (78.64%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    // int dp[21][2001];
-    vector<vector<int>> dp;
+    int dp[21][2001];
+    // vector<vector<int>> dp;
     
     int solve(vector<int> &nums, int target, int index){
         if(target==0 and index == nums.size()){
@@ -10,9 +10,9 @@ public:
         if(index == nums.size()){
             return 0;
         }
-        if(nums[index] == 0){
-            return dp[index][1000+target] = 2 * solve(nums,target,index+1);
-        }
+        // if(nums[index] == 0){
+        //     return dp[index][1000+target] = 2 * solve(nums,target,index+1);
+        // }
         if(dp[index][1000+target]!=-1){
             return dp[index][1000+target];
         }
@@ -27,18 +27,19 @@ public:
         // A - B = diff
         // Find target = (sum+target)/2 from the nums. add +1 when subsequence found
         // same as target sum problem but elements can be 0 and target sum can be -ve
-        // if target is negative, we cannot call dp[i][target]. so add 1000 to target each time we call
         int sum = 0;
         for(auto i:nums){
             sum += i;
         }
         // if sum+diff is odd, it means subset satisfying the conds cannot be found, so count = 0
-        if((sum+diff)%2 != 0){
+        if(diff>sum || (sum+diff)%2 != 0){
             return 0;
         }
         int target = (sum+diff)/2;
-        // memset(dp,-1,sizeof(dp));
-        dp.resize(nums.size()+1, vector<int>(1000+target+1, -1));
+        // if target is negative, we cannot call dp[i][target]. so add 1000 to target each time we call dp
+        // we change -1000 to 1000 range => 0 to 2001 range
+        memset(dp,-1,sizeof(dp));
+        // dp.resize(nums.size()+1, vector<int>(1000+target+1, -1));
         int ans = solve(nums,target,0);
         return ans;
     }
