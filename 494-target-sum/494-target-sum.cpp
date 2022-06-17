09ec1c0cd279a93f8d90feb6ab42f5e39ae248ec
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-18 00:38:39 +0530

    Time: 15 ms (75.10%), Space: 23.8 MB (23.18%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,24 +1,35 @@
 class Solution {
 public:
-    int dp[21][2000];
+    vector<vector<int>> dp;
     
-    int solve(vector<int> &nums, int sum, int target, int index){
-        if(sum == target and index == nums.size()){
-            cout<<index<<endl;
+    int solve(vector<int> &nums, int target, int index){
+        if(target == 0 && index == nums.size()){
+            // cout<<index<<endl;
             return 1;
         }
         if(index == nums.size()){
             return 0;
         }
-        if(dp[index][1000+sum]!=-1){
-            return dp[index][1000+sum];
+        if(dp[index][1000+target]!=-1){
+            return dp[index][1000+target];
         }
-        return dp[index][1000+sum] = solve(nums,sum-nums[index],target,index+1) + solve(nums,sum+nums[index],target,index+1);
+        if(nums[index]<=target){            
+            return dp[index][1000+target] = solve(nums,target,index+1) + solve(nums,target-nums[index],index+1);
+        }
+        return dp[index][1000+target] = solve(nums,target,index+1);
     }
     
-    int findTargetSumWays(vector<int>& nums, int target) {
-        memset(dp,-1,sizeof(dp));
-        int ans = solve(nums,0,target,0);
+    int findTargetSumWays(vector<int>& nums, int diff) {
+        int sum = 0;
+        for(auto i:nums){
+            sum += i;
+        }
+        if(diff>sum || (sum+diff)%2 == 1){
+            return 0;
+        }
+        int target = (sum+diff)/2;
+        dp.resize(nums.size()+1, vector<int> (1000+target+1,-1));
+        int ans = solve(nums,target,0);
         return ans;
     }
 };
