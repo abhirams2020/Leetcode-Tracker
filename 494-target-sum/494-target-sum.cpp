21e4fe299e1813da6070abbb593e57aada3b33fc
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-18 00:45:27 +0530

    Time: 13 ms (79.99%), Space: 9.3 MB (55.96%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
+    int dp[21][2001];
     
     int solve(vector<int> &nums, int target, int index){
-        if(target == 0 && index == nums.size()){
-            // cout<<index<<endl;
+        if(target==0 and index == nums.size()){
             return 1;
         }
         if(index == nums.size()){
@@ -13,22 +12,28 @@ public:
         if(dp[index][1000+target]!=-1){
             return dp[index][1000+target];
         }
-        if(nums[index]<=target){            
-            return dp[index][1000+target] = solve(nums,target,index+1) + solve(nums,target-nums[index],index+1);
-        }
+        if(nums[index]<=target)
+            return dp[index][1000+target] = solve(nums,target-nums[index],index+1) + solve(nums,target,index+1);
         return dp[index][1000+target] = solve(nums,target,index+1);
     }
     
+    
     int findTargetSumWays(vector<int>& nums, int diff) {
+        memset(dp,-1,sizeof(dp));
+        // A + B = sum
+        // A - B = diff
+        // Find target = (sum+target)/2 from the nums. add +1 when subsequence found
+        // same as target sum problem but elements can be 0 and target sum can be -ve
+        // if target is negative, we cannot call dp[i][target]. so add 1000 to target each time we call
         int sum = 0;
         for(auto i:nums){
             sum += i;
         }
-        if(diff>sum || (sum+diff)%2 == 1){
+        // if sum+diff is odd, it means subset satisfying the conds cannot be found, so count = 0
+        if((sum+diff)%2 != 0){
             return 0;
         }
         int target = (sum+diff)/2;
-        dp.resize(nums.size()+1, vector<int> (1000+target+1,-1));
         int ans = solve(nums,target,0);
         return ans;
     }
