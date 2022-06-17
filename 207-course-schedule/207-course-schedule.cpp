9903e5032f7f371fbb48783c549388bb43b68da6
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-17 13:17:27 +0530

    Time: 31 ms (52.86%), Space: 15.1 MB (19.43%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -3,44 +3,52 @@ public:
     // mapping of a course to its prerequisites
     unordered_map<int,vector<int>> mp;
     // visited set for checking if node visited again during dfs
-    unordered_set<int> visited;
-    // noCycle set to save valid subjects
-    unordered_set<int> noCycle;
+    // 0 -> node not visited
+    // 1 -> node visited
+    // 2 -> node visited and the course can be taken
+    vector<int> visited;
     
     bool isCycle(int curr){
         // if curr visited again means curr is starting of cycle
-        if(visited.count(curr)){
+        if(visited[curr]==1){
             return true;
         }
-        // if curr element is valid subject, there is no cycle
-        if(noCycle.count(curr)){
+        // if curr element is valid subject and it has visited before
+        if(visited[curr]==2){
             return false;
         }
+        
         // backtracking
-        visited.insert(curr);
+        // current node is added to visited list
+        visited[curr] = 1;
         for(auto i:mp[curr]){
             if(isCycle(i)){
                 return true;
             }
             else {
                 // since no cycles found for i, add to noCycle set
-                noCycle.insert(i);
+                visited[i] = 2;
             }
         }
-        visited.erase(curr);
-        // since no cycles found for curr, add to noCycle set
-        noCycle.insert(curr);
+        // since no cycles found for curr, visited[curr] = 2
+        visited[curr] = 2;
         return false;
     }
     
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        // initialise all nodes to 0 meaning unvisited
+        visited.resize(numCourses,0);
+        // total number of courses is numCourses -> from 0 to n-1
+        // assing empty vector to all courses at first, then add prerequisites to the courses
+        for(int i=0;i<numCourses;i++){
+            mp[i] = {};
+        }
         for(auto i:prerequisites){
             mp[i[0]].push_back(i[1]);
         }
         // DFS with backtracking to check if the graph contains cycle
-        for(auto i:prerequisites){
-            int curr = i[0];
-            if(isCycle(curr)){
+        for(int i=0;i<numCourses;i++){
+            if(isCycle(i)){
                 return false;
             }
         }
