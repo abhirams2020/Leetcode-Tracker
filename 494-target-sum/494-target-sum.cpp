267dedbf8967298fb0baa8de03af0967628dd4b1
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-18 01:14:22 +0530

    Time: 7 ms (96.06%), Space: 9.2 MB (61.43%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -3,8 +3,8 @@ public:
     int dp[21][2001];
     // vector<vector<int>> dp;
     
-    // solve(target,index) return no of ways to reach target from index
-    int solve(vector<int> &nums, int target, int index){
+    // targetSum(target,index) return no of ways to reach target from index
+    int targetSum(vector<int> &nums, int target, int index){
         // we need to traverse till end of nums even if target = 0 is reached
         // if we break when target is reached, we wont be considering 0's in the array
         if(index == nums.size()){
@@ -18,8 +18,8 @@ public:
         }
         if(nums[index]<=target)
             // in case of 0, no of ways = 2 * solve(nums,target,index+1)
-            return dp[index][1000+target] = solve(nums,target-nums[index],index+1) + solve(nums,target,index+1);
-        return dp[index][1000+target] = solve(nums,target,index+1);
+            return dp[index][1000+target] = targetSum(nums,target-nums[index],index+1) + targetSum(nums,target,index+1);
+        return dp[index][1000+target] = targetSum(nums,target,index+1);
     }
     
     
@@ -33,7 +33,7 @@ public:
             sum += i;
         }
         // if sum+diff is odd, it means subset satisfying the conds cannot be found, so count = 0
-        if(diff>sum || (sum+diff)%2 != 0){
+        if((sum+diff)%2 != 0){
             return 0;
         }
         int target = (sum+diff)/2;
@@ -41,7 +41,7 @@ public:
         // we change -1000 to 1000 range => 0 to 2001 range
         memset(dp,-1,sizeof(dp));
         // dp.resize(nums.size()+1, vector<int>(1000+target+1, -1));
-        int ans = solve(nums,target,0);
+        int ans = targetSum(nums,target,0);
         return ans;
     }
 };
