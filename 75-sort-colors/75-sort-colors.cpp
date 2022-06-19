f5e375e4f1d83466c37ac703f2b00feb46805c7f
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-20 02:04:31 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (70.35%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,7 +1,13 @@
 class Solution {
 public:
     void sortColors(vector<int>& arr) {
+        // at every instance after swapping,
+        // 0 to l-1 will have 0
+        // l to i will have 1
+        // r+1 to n-1 will have 2
         int l=0, r=arr.size()-1;
+        // since we dont move i when arr[i]==2, we can be sure that if we are at i, 0 to i-1 will not contain 2.
+        // if arr[i]==0, we swap 0 with 1 at index l. so for arr[i]==0 and arr[i]==1, do i++.
         for(int i=0; i<=r; ){
             if(arr[i]==0){
                 swap(arr[i],arr[l]);
