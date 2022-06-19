2814b88567a110271b44b5ed68b6c4db6d8c380d
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-19 11:21:14 +0530

    Time: 1068 ms (33.33%), Space: 265.3 MB (66.67%) - LeetHub

--- 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.cpp
+++ 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -1,28 +1,52 @@
 class Solution {
 public:
-    int solve(string s, int index, int sum){
-        int len = 0;
-        int p = 0;
-        for(int i=index;i<=s.length();i++){
-            int val = s[i]-'0';
-            long double curr = val * pow(2,p);
-            if(val==1 && curr<=sum){
-                sum-=curr;
-                len++;
-            }
-            else if(val==0){
-                len++;
-            }
-            p++;
+    // int solve(string s, int index, int sum){
+    //     int len = 0;
+    //     int p = 0;
+    //     for(int i=index;i<s.length();i++){
+    //         // cout<<index<<" "<<sum<<endl;
+    //         int val = s[i]-'0';
+    //         long double curr = val * pow(2,p);
+    //         if(curr<=sum){
+    //             sum-=curr;
+    //             len++;
+    //         }
+    //         p++;
+    //     }
+    //     return len;
+    // }
+    // int longestSubsequence(string s, int sum) {
+    //     reverse(s.begin(),s.end());
+    //     int ans = 0;
+    //     for(int i=0;i<s.length();i++){
+    //         ans = max(ans, solve(s,i,sum));
+    //     }
+    //     return ans;
+    // }
+    
+    vector<vector<int>> dp;
+    
+    int solve(string &s, int sum, int size, int index){
+        if(index==s.length()){
+            return 0;
         }
-        return len;
+        if(dp[size][index]!=-1){
+            return dp[size][index];
+        }
+        int val = s[index]-'0';
+        long double curr = val * pow(2,size);
+        int include = 0, not_include = 0;
+        if(curr <= sum){
+            include = 1 + solve(s,sum-curr,size+1,index+1);
+        }
+        not_include = solve(s,sum,size,index+1);
+        return dp[size][index] = max(include, not_include);
     }
-    int longestSubsequence(string s, int sum) {
+    
+    int longestSubsequence(string s, int k) {
+        int n = s.length();
+        dp.resize(n+1, vector<int> (n+1,-1));
         reverse(s.begin(),s.end());
-        int ans = 0;
-        for(int i=0;i<s.length();i++){
-            ans = max(ans, solve(s,i,sum));
-        }
-        return ans;
+        return solve(s,k,0,0);
     }
 };
