e153852fd81d7a47a4439aae5d31a45485f4d41f
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-19 11:56:56 +0530

    Time: 573 ms (33.33%), Space: 6.7 MB (100.00%) - LeetHub

--- 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.cpp
+++ 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
 //     // BRUTE FORCE APPROACH
-    int solve(string s, int index, int sum){
+    int solve(string &s, int index, int sum){
         int len = 0;
         int p = 0;
         for(int i=index;i<s.length();i++){
-            int val = s[i]-'0';
-            long double curr = pow(2,p);
-            if(val==1 && curr<=sum){
-                sum-=curr;
-                len++;
-            }
-            else if(val==0){
+            int curr = s[i]-'0';
+            long double val= curr * pow(2,p);
+            if(val<=sum){
+                sum-=val;
                 len++;
             }
             p++;
@@ -30,7 +27,7 @@ public:
     
 //     // USING DP (not sure how its not 3D dp)
 //     vector<vector<int>> dp;
-    
+
 //     int solve(string &s, int sum, int len, int index){
 //         if(index==s.length()){
 //             return 0;
@@ -38,16 +35,16 @@ public:
 //         if(dp[len][index]!=-1){
 //             return dp[len][index];
 //         }
-//         int val = s[index]-'0';
-//         long double curr = val * pow(2,len);
+//         int curr = s[index]-'0';
+//         long double val = curr * pow(2,len);
 //         int include = 0, not_include = 0;
-//         if(curr <= sum){
-//             include = 1 + solve(s,sum-curr,len+1,index+1);
+//         if(val <= sum){
+//             include = 1 + solve(s,sum-val,len+1,index+1);
 //         }
 //         not_include = solve(s,sum,len,index+1);
 //         return dp[len][index] = max(include, not_include);
 //     }
-    
+
 //     int longestSubsequence(string s, int k) {
 //         int n = s.length();
 //         dp.resize(n+1, vector<int> (n+1,-1));
