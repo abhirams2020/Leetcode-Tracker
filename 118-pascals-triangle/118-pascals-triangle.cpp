c597624c77bee33b9c5c1f1da238cad8605579b0
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-20 00:55:06 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (90.78%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -7,6 +7,7 @@ public:
             vector<int> temp;
             temp.push_back(1);
             // curr_level will have prev_level+1 elements between 1 to 1
+            // from index=1, no of elements bw 1 to 1 in i'th row = i-1
             for(int j=0;j<i-1;j++){
                 temp.push_back(ans[i-1][j]+ans[i-1][j+1]);
             }
