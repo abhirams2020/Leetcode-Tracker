ea894dc871bcac0c45f0105f1ea17336bfa1599e
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-20 02:23:57 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (29.16%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -4,6 +4,7 @@ public:
         int end = m+n-1;
         int p1 = m-1;
         int p2 = n-1;
+        // compare p1 and p2 and add max to end of nums1
         while(p1>=0 && p2>=0){
             if(nums1[p1] > nums2[p2]){
                 nums1[end] = nums1[p1];
@@ -15,11 +16,13 @@ public:
             }
             end--;
         }
+        // if p1 has any element left, add to end
         while(p1>=0){
             nums1[end] = nums1[p1];
             p1--;
             end--;
         }
+        // if p2 has any element left, add to end
         while(p2>=0){
             nums1[end] = nums2[p2];
             p2--;
