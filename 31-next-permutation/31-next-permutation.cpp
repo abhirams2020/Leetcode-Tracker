f6dc92016df00b3d189aea5d7302e1a2849c3fcf
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-21 01:59:52 +0530

    Time: 0 ms (100.00%), Space: 12.2 MB (36.57%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -3,6 +3,8 @@ public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
         int p = -1;
+        
+        // find pivot element - first element which is decreasing from right of array
         for(int i=n-2;i>=0;i--){
             if(nums[i]<nums[i+1]){
                 p = i;
@@ -15,13 +17,16 @@ public:
             return;
         }
         
-        // find first no from right greater than nums[p]
+        // find first no from right greater than nums[p] and swap both and exit the loop.
         for(int i=n-1;i>p;i--){
             if(nums[i]>nums[p]){
                 swap(nums[i],nums[p]);
                 break;
             }
         }
+        
+        // sort p+1 to end. since we swap with number just greater than nums[p], p+1 to end is in descending order
+        // so instead of sorting, just reverse the range and return
         reverse(nums.begin()+p+1,nums.end());
         return;
     }
