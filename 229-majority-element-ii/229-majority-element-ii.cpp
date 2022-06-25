552b4f31ee7b6782e8847138cec71ddb16807b7f
Abhiram S <58247649+abhirams2020@users.noreply.github.com>
2022-06-25 11:58:18 +0530

    Time: 12 ms (89.59%), Space: 15.8 MB (82.00%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -17,12 +17,17 @@ public:
         int maj1 = -1, maj2 = -1;
         int count1 = 0, count2 = 0;
         for(auto i:nums){
+            // ALGO : think like counting votes. each favourable vote improves probability to win.
+            // each unfavourable vote reduces prob to win. after a point if no probability, change candidate
+            
+            // CASE 1 : current element is same as majority (increase priority of majority candidate)
             if(maj1==i){
                 count1++;
             }
             else if(maj2==i){
                 count2++;
             }
+            // CASE 2 : majority candidate priority is 0. so choose the new candidate over it.
             else if(count1==0){
                 maj1 = i;
                 count1 = 1;
@@ -31,6 +36,7 @@ public:
                 maj2 = i;
                 count2 = 1;
             }
+            // CASE 3 : current element is not same as majority (decrease priority of candidate)
             else {
                 count1--;
                 count2--;
